dedb19e35446f7825a744f9011d07bd126aa9234
JIHU96 <korjihu@gmail.com>
2024-09-07 00:52:18 +0900

    feat: FNB 메뉴바에 아이콘 적용 [#3]

--- src/components/layout/FooterNavBar.tsx
+++ src/components/layout/FooterNavBar.tsx
@@ -1,12 +1,26 @@
-import { useLocation, useParams } from 'react-router-dom';
+import IconCommunity from '../../assets/icons/icon-community.svg';
+import IconProduct from '../../assets/icons/icon-product.svg';
+import IconHome from '../../assets/icons/icon-home.svg';
+import IconChat from '../../assets/icons/icon-chat.svg';
+import IconAccount from '../../assets/icons/icon-account.svg';
+import Menu from './components/Menu';
 
 const FooterNavBar = () => {
-  const param = useParams();
-  const location = useLocation();
+  const menus = [
+    { name: '커뮤니티', to: '/community', iconComponent: <IconCommunity /> },
+    { name: '모든 상품', to: '/product', iconComponent: <IconProduct /> },
+    { name: '홈', to: '/', iconComponent: <IconHome /> },
+    { name: '채팅', to: '/chatting', iconComponent: <IconChat /> },
+    { name: '마이페이지', to: '/mypage', iconComponent: <IconAccount /> },
+  ];
 
-  console.log(param, location);
-
-  return <footer className="w-full h-[72px] bg-orange-500">FooterNavBar</footer>;
+  return (
+    <footer className="w-full h-[72px] grid grid-cols-5 border-t shrink-0 border-black">
+      {menus.map(({ name, to, iconComponent }, idx) => (
+        <Menu key={idx} to={to} iconComponent={iconComponent} name={name} />
+      ))}
+    </footer>
+  );
 };
 
 export default FooterNavBar;
---
+++ src/components/layout/components/Menu.tsx
@@ -0,0 +1,26 @@
+import { Link, useLocation } from 'react-router-dom';
+
+interface MenuProps {
+  to: string;
+  iconComponent: JSX.Element;
+  name: string;
+}
+
+const Menu = ({ to, iconComponent, name }: MenuProps) => {
+  const { pathname } = useLocation();
+
+  /* TDOD: 메뉴 아이콘 활성화 색상 나중에 디자인 시스템으로 변경 */
+  return (
+    <Link
+      to={to}
+      className={`flex flex-col justify-center items-center text-black hover:text-green-300   ${
+        pathname === to && 'text-green-300'
+      }`}
+    >
+      {iconComponent}
+      {name}
+    </Link>
+  );
+};
+
+export default Menu;
