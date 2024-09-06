594e9042e8483ffc2ae00cf2ab2a042d31be2ad9
JIHU96 <korjihu@gmail.com>
2024-09-07 00:03:28 +0900

    feat: 헤더에 앱 로고와 검색 아이콘 추가 -> 각각 홈과 검색 페이지로 url 이동함 [#3]

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,22 @@
+import logoImg from '../../assets/logo.png';
+import IconSearch from '../../assets/icons/icon-search.svg';
+import { Link } from 'react-router-dom';
+
 const Header = () => {
-  return <header className="w-full h-14 bg-orange-300">header</header>;
+  return (
+    <header className="w-full h-14 px-2 flex items-center justify-between">
+      <h1 className="w-full h-full flex items-center">
+        <Link to={'/'} className="block h-3/4">
+          <img src={logoImg} alt="fresh to you 로고" className="h-full max-w-full rounded-full" />
+          <span className="a11y-hidden">1</span>
+        </Link>
+      </h1>
+
+      <Link to={'/search'}>
+        <IconSearch />
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
