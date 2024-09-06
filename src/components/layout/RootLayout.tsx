64cf77336d5477e0cb290bbac8bf6c040d8244ed
JIHU96 <korjihu@gmail.com>
2024-09-06 22:24:54 +0900

    fix: RootLayout에서 Outlet에 width와 height를 100%로 가지는 main 태그로 랩핑 [#3]
    - 레이아웃으로 적용할 모든 페이지에 대해 메인 컨텐츠는 남는 영역 전부 차지하게 하기 위함

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import FooterNavBar from './FooterNavBar';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import FooterNavBar from './FooterNavBar';
 
 const RootLayout = () => {
   return (
-    <div className="w-full h-full flex flex-col justify-between">
+    <div className="w-full h-full flex flex-col justify-betwee">
       <Header />
-      <Outlet />
+      <main className="w-full h-full">
+        <Outlet />
+      </main>
+
       <FooterNavBar />
     </div>
   );
