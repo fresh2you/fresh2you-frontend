e4332cbf37d7ba010ee3031c2904fda3c6c4e8ed
JIHU96 <korjihu@gmail.com>
2024-09-07 15:42:45 +0900

    feat: 홈페이지 UI 구현 [#3]
    - 홈페이지 용 목데이터 추가
    - 배너와 제품 카드 컴포넌트 추가
    - 가로 스크롤 적용

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,11 @@
 import Banner from './component/Banner';
+import { recommendedProcuts } from '../../mockdata/homeMockData';
 
 const HomePage = () => {
   return (
     <div className="w-full h-full">
-      HomePage
-      <Banner />
+      <Banner title="이런 상품은 어떠세요?" products={recommendedProcuts.products} />
+      <Banner title="이런 상품은 어떠세요?" products={recommendedProcuts.products} />
     </div>
   );
 };
--- src/pages/home/component/Banner.tsx
+++ src/pages/home/component/Banner.tsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import ProductCard from './ProductCard';
+import { ProductCardProps } from './ProductCard';
 
-const Banner = () => {
-  return <div>Banner</div>;
+interface BannerProps {
+  title: string;
+  products: ProductCardProps[];
+}
+
+const Banner = ({ title = 'banner title', products = [] }: BannerProps) => {
+  console.log(products);
+
+  return (
+    <section className="w-full h-auto px-4 py-4 flex flex-col items-center gap-4">
+      <div className="font-bold">{title}</div>
+
+      <div className="w-full grid grid-rows-2 gap-4" style={{ gridTemplateRows: '1fr 1fr' }}>
+        <article className="w-full h-auto flex lg:justify-center gap-4 overflow-x-auto scrollbar-hide">
+          {products.slice(0, 5).map(({ product_id, name, seller, description, price, img }) => (
+            <ProductCard
+              key={product_id}
+              product_id={product_id}
+              name={name}
+              seller={seller}
+              description={description}
+              price={price}
+              img={img}
+            />
+          ))}
+        </article>
+
+        {/* 5개 이상일 경우 아래줄로 보냄 */}
+        {products.length > 5 && (
+          <article className="w-full h-auto flex lg:justify-center gap-4 overflow-x-auto scrollbar-hide">
+            {products.slice(5).map(({ product_id, name, seller, description, price, img }) => (
+              <ProductCard
+                key={product_id}
+                product_id={product_id}
+                name={name}
+                seller={seller}
+                description={description}
+                price={price}
+                img={img}
+              />
+            ))}
+          </article>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
