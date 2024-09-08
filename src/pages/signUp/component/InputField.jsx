b515676a1540e9ba1c694eb291ed2b49bd54b6d8
choycoy <passionfruits479@gmail.com>
2024-09-09 02:04:50 +0900

    스타일링 변경

--- src/pages/signUp/component/InputField.jsx
+++ src/pages/signUp/component/InputField.jsx
@@ -1,6 +1,6 @@
 export default function InputField({ label, type, value, setValue, placeholder, autoComplete, feedback }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col flex-start">
       <label htmlFor={label} className="font-semibold mb-1" style={{ color: '#333333' }}>
         {label}
       </label>
@@ -11,7 +11,7 @@ export default function InputField({ label, type, value, setValue, placeholder, 
         onChange={(e) => setValue(e.target.value)}
         placeholder={placeholder}
         autoComplete={autoComplete}
-        className="h-10 rounded px-1 text-custom-black border outlin-none w-72 "
+        className="h-10 rounded px-1 text-custom-black border outline-none w-72 "
         style={{ borderColor: '#D3D3D3', outline: 'none' }}
       />
       {feedback && (
