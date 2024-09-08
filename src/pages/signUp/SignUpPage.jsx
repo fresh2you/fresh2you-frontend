6489b7d304a18b196dd5681be46f3c18e896dccc
choycoy <passionfruits479@gmail.com>
2024-09-09 02:08:05 +0900

    코드 간결화

--- src/pages/signUp/SignUpPage.jsx
+++ src/pages/signUp/SignUpPage.jsx
@@ -1,24 +1,19 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useFunnel } from '@use-funnel/react-router-dom';
-import InputField from './component/InputField';
 import { getStepsConfig } from './stepsConfig';
-import {
-  checkEmailDuplicate,
-  checkNicknameDuplicate,
-  handleConfirmPasswordChange,
-  mockCheckEmailDuplicate,
-  mockCheckNicknameDuplicate,
-  isFormValid,
-  validatePassword,
-} from './utils/validationUtils';
-import blockIcon from '../../assets/icons/block.svg';
-import '../../styles/styles.css';
-import { handleSubmit } from './utils/handleSubmit';
-import NextButton from './component/NextButton';
-import ErrorMessages from './component/ErrorMessage';
+import { handleSubmit } from './utils/handlers/handleSubmit';
+import ErrorMessages from './component/ErrorMessages';
 import PasswordFeedback from './component/PasswordFeedback';
 import StepFields from './component/StepFields';
-
+import { VerificationModal } from './component/VerificationModal';
+import { validationLogic } from './utils/validationLogic';
+import { handleConfirmPasswordChange } from './utils/passwordUtils';
+import SubmitBtn from './component/buttons/SubmitBtn';
+import {
+  useResetStatusOnEmailChange,
+  useResetStatusOnNicknameChange,
+  useResetStatusOnTelChange,
+} from './hooks/useResetStatusOnFieldChange';
 export default function SignUpPage() {
   const funnel = useFunnel({
     id: 'user-signup',
@@ -29,115 +24,81 @@ export default function SignUpPage() {
         password: '',
         nickname: '',
         confirmPassword: '',
+        phoneNo: '',
       },
     },
   });
-
   const [formData, setFormData] = useState({
     email: funnel.context.email,
     password: funnel.context.password,
     confirmPassword: funnel.context.confirmPassword,
+    phoneNo: funnel.context.phoneNo,
     nickname: funnel.context.nickname,
   });
   const [status, setStatus] = useState({
     emailStatus: '',
     passwordStatus: '',
+    phoneNoStatus: '',
     nicknameStatus: '',
+    requestStatus: '',
   });
   const [validity, setValidity] = useState({
     isEmailValid: false,
     isPasswordCombinationValid: false,
     isConfirmPasswordValid: false,
+    isPhoneNoValid: false,
+    isPhoneNoVerified: false,
     isNicknameValid: false,
   });
   const [passwordFeedbacks, setPasswordFeedbacks] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [sendAttempts, setSendAttempts] = useState(0);
+
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+
+  useResetStatusOnEmailChange(formData.email, setStatus);
+  useResetStatusOnNicknameChange(formData.nickname, setStatus);
+  useResetStatusOnTelChange(formData.phoneNo, setStatus);
 
   useEffect(() => {
-    setValidity({
-      isEmailValid: !Boolean(status.emailStatus) && !!formData.email,
-      isPasswordCombinationValid: validatePassword(formData.password).isValid,
-      isConfirmPasswordValid:
-        !Boolean(status.passwordStatus) &&
-        !!formData.password &&
-        !!formData.confirmPassword &&
-        formData.confirmPassword === formData.password,
-      isNicknameValid: !Boolean(status.nicknameStatus) && !!formData.nickname,
-    });
+    validationLogic.updateValidity(formData, status, setValidity, funnel);
   }, [status, formData]);
-  console.log(validity);
 
   useEffect(() => {
-    if (formData.email !== funnel.context.email) {
-      setValidity((prev) => ({
-        ...prev,
-        isEmailValid: false,
-      }));
-    }
-  }, [formData.email, funnel.context.email]);
-
-  useEffect(() => {
-    const passwordFeedbackMessages = [];
-    if (formData.password) {
-      const passwordValidation = validatePassword(formData.password);
-
-      if (!passwordValidation.minLengthValid) passwordFeedbackMessages.push('비밀번호는 최소 8자 이상이어야 합니다.');
-      if (!passwordValidation.hasUpperCase) passwordFeedbackMessages.push('비밀번호에 대문자를 포함해야 합니다.');
-      if (!passwordValidation.hasNumber) passwordFeedbackMessages.push('비밀번호에 숫자를 포함해야 합니다.');
-      if (!passwordValidation.hasSpecialCharacter)
-        passwordFeedbackMessages.push('비밀번호에 특수문자를 포함해야 합니다.');
-    }
-
+    const passwordFeedbackMessages = validationLogic.getPasswordFeedbackMessages(formData.password);
     setPasswordFeedbacks(passwordFeedbackMessages);
-  }, [formData.password]);
-
-  useEffect(() => {
-    handleConfirmPasswordChange(formData.confirmPassword, formData.password, formData, setFormData, status, setStatus);
+    if (formData.confirmPassword) {
+      handleConfirmPasswordChange(formData.confirmPassword, formData.password, setFormData, setStatus);
+    }
   }, [formData.password, formData.confirmPassword]);
 
-  useEffect(() => {
-    if (formData.nickname !== funnel.context.nickname) {
-      setValidity((prev) => ({
-        ...prev,
-        isNicknameValid: false,
-      }));
-    }
-  }, [formData.nickname, funnel.context.nickname]);
-
-  const steps = getStepsConfig(
-    formData,
-    setFormData,
-    status,
-    setStatus,
-    mockCheckEmailDuplicate,
-    mockCheckNicknameDuplicate,
-    handleConfirmPasswordChange,
-    validity,
-  );
+  const steps = getStepsConfig(formData, setFormData, setStatus, validity, handleOpenModal, sendAttempts);
   const currentStep = funnel.step;
 
   return (
     <div className="flex">
       <form
         onSubmit={(e) => handleSubmit(e, validity, formData)}
-        className="flex flex-col justify-center min-h-screen bg-gray-100 my-0 mx-auto"
-        style={{ minWidth: '376px' }}
+        className="flex flex-col justify-center min-h-screen bg-gray-100 my-0 mx-auto items-start"
+        style={{ width: '376px' }}
       >
         <PasswordFeedback passwordFeedbacks={passwordFeedbacks} />
-        <ErrorMessages status={status} />
+        <ErrorMessages status={status} sendAttempts={sendAttempts} />
         <StepFields steps={steps} currentStep={currentStep} validity={validity} funnel={funnel} formData={formData} />
-        {currentStep === '닉네임입력' && (
-          <button
-            type="submit"
-            className="px-4 py-2 rounded submit-button"
-            style={{
-              outline: 'none',
-              borderColor: 'transparent',
-            }}
-          >
-            제출
-          </button>
-        )}
+        {currentStep === '닉네임입력' && <SubmitBtn validity={validity} />}
       </form>
+      {isModalOpen && (
+        <VerificationModal
+          onClose={handleCloseModal}
+          phoneNo={formData.phoneNo}
+          validity={validity}
+          setValidity={setValidity}
+          setStatus={setStatus}
+          sendAttempts={sendAttempts}
+          setSendAttempts={setSendAttempts}
+        />
+      )}
     </div>
   );
 }
