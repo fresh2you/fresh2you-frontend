33a28912001086a8c45765c3dcb0d8d8e3b0e609
choycoy <passionfruits479@gmail.com>
2024-09-09 02:02:56 +0900

    제출 버튼 컴포넌트 생성

---
+++ src/pages/signUp/component/buttons/SubmitBtn.jsx
@@ -0,0 +1,20 @@
+import { isFormValid } from '../../utils/validationUtils';
+import blockIcon from '../../../../assets/icons/block.svg';
+const SubmitBtn = (validity) => {
+  return (
+    <button
+      type="submit"
+      className="px-4 py-2 rounded submit-button bg-custom-green mt-6 w-full"
+      disabled={!isFormValid(validity)}
+      style={{
+        color: 'white',
+        outline: 'none',
+        borderColor: 'transparent',
+        cursor: !isFormValid(validity) ? `url(${blockIcon}), auto` : 'pointer',
+      }}
+    >
+      제출
+    </button>
+  );
+};
+export default SubmitBtn;
