a86c9213ad7a918ca4770f944ce197882057c7ea
choycoy <passionfruits479@gmail.com>
2024-09-09 02:10:01 +0900

    스타일링 변경

--- src/pages/signUp/component/PasswordFeedback.jsx
+++ src/pages/signUp/component/PasswordFeedback.jsx
@@ -2,7 +2,7 @@ const PasswordFeedback = ({ passwordFeedbacks }) => {
   if (passwordFeedbacks.length === 0) return null;
 
   return (
-    <div className="text-custom-gray-dark bg-custom-gray-light px-2 py-2 rounded mb-2 text-sm">
+    <div className="text-custom-black bg-custom-gray-light px-2 py-2 rounded mb-2 text-sm">
       {passwordFeedbacks.map((feedback, index) => (
         <p key={index}>•{feedback}</p>
       ))}
