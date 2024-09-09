4746ad778018c5e6145b45f234e339da3c5dfdbc
choycoy <passionfruits479@gmail.com>
2024-09-09 21:06:42 +0900

    이미지 경로 변경

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import logoImg from '../../assets/logo.png';
+import logoImg from '../../assets/img/logo.png';
 import IconSearch from '../../assets/icons/icon-search.svg';
 import { Link } from 'react-router-dom';
 
