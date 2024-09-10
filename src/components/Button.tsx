f36eb6a5197eb804c150412a85aeba36e12f83ee
choycoy <passionfruits479@gmail.com>
2024-09-10 16:14:04 +0900

    홈버튼 공용 컴포넌트 사용

--- src/components/Button.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-/* TODO: 임시 공용 버튼 생성 */
-const Button = () => {
-  return <button>Button</button>;
-};
-
-export default Button;
---
+++ src/components/HomeButton.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+
+const HomeButton = ({ className }) => {
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate('/');
+  };
+
+  return (
+    <button
+      onClick={handleGoHome}
+      className={`px-4 py-2 text-base md:text-lg font-semibold rounded-lg ${className} transition`}
+      style={{ borderColor: 'transparent', outline: 'none' }}
+    >
+      홈으로 이동
+    </button>
+  );
+};
+
+export default HomeButton;
