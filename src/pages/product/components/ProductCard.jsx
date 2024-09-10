fe12cbfd719f8e8d1d1deffa70a45309e15bba39
choycoy <passionfruits479@gmail.com>
2024-09-11 01:42:31 +0900

    전체 페이지 상품 카드 컴포넌트 생성

--- src/pages/product/components/ProductCard.jsx
+++ src/pages/product/components/ProductCard.jsx
@@ -1,17 +1,31 @@
-const ProductCard = ({ product }) => (
-  <div className="bg-custom-white shadow-md rounded-lg overflow-hidden w-full max-w-xs">
-    <div className="flex justify-center items-center w-full h-48">
-      <img src={product.img} alt={product.name} className="object-contain w-48 h-48" />
+import { forwardRef } from 'react';
+import { formatCurrency } from '../../../utils/commonUtils';
+import { useNavigate } from 'react-router-dom';
+
+const ProductCard = forwardRef(({ product }, ref) => {
+  const navigate = useNavigate();
+  return (
+    <div
+      ref={ref}
+      className="bg-custom-white shadow-lg rounded-xl overflow-hidden w-full max-w-xs border border-custom-gray-light h-[296px]"
+    >
+      <div className="flex justify-center items-center w-full mt-4">
+        <img src={product.img} alt={product.name} className="object-contain w-28 h-28" />
+      </div>
+      <div className="p-4">
+        <h2 className="text-xl font-semibold">{product.name}</h2>
+        <p className="text-xl text-custom-gray-dark">{product.seller}</p>
+        <p className="text-custom-green font-semibold text-xl">{formatCurrency(product.price)} 원</p>
+        <button
+          className="mt-2 w-full bg-custom-green text-white py-2 rounded-lg hover:bg-custom-green-hover transition font-semibold"
+          style={{ borderColor: 'transparent', outline: 'none' }}
+          onClick={() => navigate(`./${product.product_id}`)}
+        >
+          구매하기
+        </button>
+      </div>
     </div>
-    <div className="p-4">
-      <h2 className="text-xl font-semibold">{product.name}</h2>
-      <h3 className="text-xl text-custom-gray-dark">
-        {product.seller}, <span className="text-custom-green font-semibold">{product.price} 원</span>
-      </h3>
-      <button className="mt-4 w-full bg-custom-green text-white py-2 rounded-lg hover:bg-custom-green-hover transition">
-        구매하기
-      </button>
-    </div>
-  </div>
-);
+  );
+});
+
 export default ProductCard;
