89d1ab141840f7bb148b16e095f76ae07d0a0b08
choycoy <passionfruits479@gmail.com>
2024-09-11 01:44:11 +0900

    전체 상품 페이지 구현

--- src/pages/product/ProductsPage.jsx
+++ src/pages/product/ProductsPage.jsx
@@ -1,44 +1,86 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect, useCallback, useRef } from 'react';
 import ProductCard from './components/ProductCard';
-import axios from 'axios';
-import { recommendedProducts } from '../../mockdata/homeMockData';
-import HomeButton from '../../components/HomeButton';
+import { mockProducts } from '../../mockdata/MockData';
+import ProductCardSkeleton from './components/ProductCardSkeleton';
+import CategoryButtons from './components/CategoryButtons';
+
 const ProductsPage = () => {
   const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const navigate = useNavigate();
-  console.log(recommendedProducts);
-  useEffect(() => {
-    // const fetchProducts = async () => {
-    //   try {
-    //     const response = await axios.get('/api/products'); // 백엔드에서 상품 데이터를 가져옴
-    //     setProducts(response.data);
-    //   } catch (error) {
-    //     console.error('상품 데이터를 불러오지 못했습니다:', error);
-    //   } finally {
-    //     setLoading(false);
-    //   }
-    // };
-    // fetchProducts();
-  }, []);
+  const [loading, setLoading] = useState(false);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const itemsPerPage = 10;
 
-  // if (loading) {
-  //   return <div className="text-center mt-20">상품 목록을 불러오는 중입니다...</div>;
-  // }
-  const productList = recommendedProducts.products;
-  if (productList.length === 0) {
+  const observer = useRef();
+
+  const fetchProducts = useCallback(
+    async (pageNumber) => {
+      if (!hasMore) return;
+
+      setLoading(true);
+      const startIndex = (pageNumber - 1) * itemsPerPage;
+      const newProducts = mockProducts.products.slice(startIndex, startIndex + itemsPerPage);
+
+      if (newProducts.length < itemsPerPage) {
+        setHasMore(false);
+      }
+
+      setProducts((prevProducts) => [...prevProducts, ...newProducts]);
+      setLoading(false);
+    },
+    [itemsPerPage, hasMore],
+  );
+
+  const lastProductRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPage) => prevPage + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore],
+  );
+
+  useEffect(() => {
+    fetchProducts(pageNumber);
+  }, [pageNumber, fetchProducts]);
+
+  if (loading || products.length === 0) {
+    return (
+      <div className="mx-auto py-8 text-custom-black max-w-7xl px-4">
+        <h1 className="text-4xl font-bold text-center mb-8 text-custom-green">갓 수확했어요!</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12 justify-items-center">
+          {Array.from({ length: itemsPerPage }).map((_, index) => (
+            <ProductCardSkeleton key={index} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  if (products.length === 0 && !loading) {
     return <div className="text-center mt-20">등록된 상품이 없습니다.</div>;
   }
 
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-center mb-8">전체 상품 목록</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {productList.map((product) => (
-          <ProductCard key={product.product_id} product={product} />
-        ))}
+    <div className="mx-auto py-8 text-custom-black max-w-7xl px-4">
+      <CategoryButtons />
+      <h1 className="text-5xl font-bold text-center my-8 text-custom-green">갓 수확했어요!</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12 justify-items-center">
+        {products.map((product, index) => {
+          const key = `${product.product_id}-${index}`;
+          if (index === products.length - 1) {
+            return <ProductCard ref={lastProductRef} key={key} product={product} />;
+          } else {
+            return <ProductCard key={key} product={product} />;
+          }
+        })}
       </div>
+      {loading && <div className="text-center mt-4">더 많은 상품을 불러오는 중입니다...</div>}
     </div>
   );
 };
