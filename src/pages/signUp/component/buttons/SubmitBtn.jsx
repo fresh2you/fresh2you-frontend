2a0af17904a03ff148824da604447ab2e97c3236
choycoy <passionfruits479@gmail.com>
2024-09-12 01:03:23 +0900

    cursor:not-allowed로 변경

--- src/pages/signUp/component/buttons/SubmitBtn.jsx
+++ src/pages/signUp/component/buttons/SubmitBtn.jsx
@@ -1,18 +1,16 @@
 import { isFormValid } from '../../utils/validationUtils';
-import blockIcon from '../../../../assets/icons/block.svg';
 import { handleSubmit } from '../../utils/handlers/handleSubmit';
 const SubmitBtn = ({ validity }) => {
   const formIsValid = isFormValid(validity);
   return (
     <button
       type="submit"
-      className="px-4 py-2 rounded submit-button bg-custom-green mt-4 w-full"
+      className="px-4 py-2 rounded submit-button bg-custom-green mt-4 w-full hover:border-transparent"
       disabled={!formIsValid}
       style={{
         color: 'white',
         outline: 'none',
-        borderColor: 'transparent',
-        cursor: formIsValid ? `pointer` : `url(${blockIcon}), auto`,
+        cursor: formIsValid ? `pointer` : `not-allowed`,
       }}
     >
       제출
