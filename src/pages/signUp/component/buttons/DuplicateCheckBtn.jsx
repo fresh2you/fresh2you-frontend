347efdf219b7aa85c730e912e78b2ec6c20bcc30
choycoy <passionfruits479@gmail.com>
2024-09-12 01:02:54 +0900

    tailwind 적용

--- src/pages/signUp/component/buttons/DuplicateCheckBtn.jsx
+++ src/pages/signUp/component/buttons/DuplicateCheckBtn.jsx
@@ -3,11 +3,9 @@ export const DuplicateCheckButton = ({ isValid, onClick }) => {
     <button
       type="button"
       onClick={onClick}
-      className="flex items-center justify-center min-w-20 h-10 ml-2 rounded-md px-2 whitespace-nowrap"
+      className="flex items-center justify-center min-w-20 h-10 ml-2 rounded-md px-2 whitespace-nowrap hover:border-transparent text-white"
       style={{
         backgroundColor: isValid ? '#7D7D7D' : '#40A578',
-        color: 'white',
-        borderColor: 'transparent',
         outline: 'none',
       }}
     >
