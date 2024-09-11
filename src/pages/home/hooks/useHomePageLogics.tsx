6008f19c50c0c5a30406dc88fff185c543e21b13
JIHU96 <korjihu@gmail.com>
2024-09-11 19:05:51 +0900

    chore: axios와 json-server 세팅에 맞게 기존 홈/검색 페이지에 적용  [#1]
    - 로컬 목데이터 파일에서 데이터를 가져오는 부분을 axios를 이용한 json-server 통신으로 변경

--- src/pages/home/hooks/useHomePageLogics.tsx
+++ src/pages/home/hooks/useHomePageLogics.tsx
@@ -1,7 +1,21 @@
-const useHomePageLogics = () => {
-  const print = console.log;
+import { useQuery } from '@tanstack/react-query';
+import { instance } from '@/instance';
+import { ProductCardProps } from '@/pages/home/component/ProductCard';
 
-  return { print };
+const useHomePageLogics = () => {
+  // 필터별로 쿼리 분리하기
+  const { data: mockProducts } = useQuery({
+    queryKey: ['mockProducts'],
+    queryFn: async () => {
+      const { data: result } = await instance.get<ProductCardProps[]>('/mockProducts');
+
+      return result;
+    },
+    enabled: true,
+    staleTime: 60 * 1000,
+  });
+
+  return { mockProducts };
 };
 
 export default useHomePageLogics;
