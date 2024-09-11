209ba295316ea95b31b96a367c6b847c9834a5c2
choycoy <passionfruits479@gmail.com>
2024-09-11 23:20:34 +0900

    tailwind 수정

--- src/pages/signUp/component/buttons/CloseBtn.jsx
+++ src/pages/signUp/component/buttons/CloseBtn.jsx
@@ -2,11 +2,8 @@ export const CloseBtn = ({ onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="absolute -top-2 -right-2 text-2xl"
+      className="absolute -top-2 -right-2 text-2xl text-white bg-transparent border-none"
       style={{
-        color: 'white',
-        backgroundColor: 'transparent',
-        border: 'none',
         outline: 'none',
       }}
     >
