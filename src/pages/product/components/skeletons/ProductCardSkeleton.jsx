86b62913f57c09499322346e820883a7729d29a5
choycoy <passionfruits479@gmail.com>
2024-09-11 23:20:53 +0900

    tailwind 수정

--- src/pages/product/components/skeletons/ProductCardSkeleton.jsx
+++ src/pages/product/components/skeletons/ProductCardSkeleton.jsx
@@ -1,5 +1,5 @@
 const ProductCardSkeleton = () => (
-  <div className="bg-custom-white shadow-lg rounded-xl overflow-hidden w-full max-w-xs border border-custom-gray-light animate-pulse">
+  <div className="bg-white shadow-lg rounded-xl overflow-hidden w-full max-w-xs border border-custom-gray-light animate-pulse">
     <div className="flex justify-center items-center w-full h-48 mt-4 bg-gray-200"></div>
     <div className="p-4">
       <div className="h-6 bg-gray-200 mb-2 rounded"></div>
