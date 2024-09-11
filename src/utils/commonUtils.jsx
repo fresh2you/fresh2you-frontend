7600bd1be640fa76423599268da6871ed93d00e0
choycoy <passionfruits479@gmail.com>
2024-09-12 02:14:47 +0900

    상품 가격 input formatter

--- src/utils/commonUtils.jsx
+++ src/utils/commonUtils.jsx
@@ -1,3 +1,8 @@
 export function formatCurrency(value) {
   return value.toLocaleString('ko-KR');
 }
+export function formatPriceInput(value) {
+  if (!value) return '';
+  const number = Number(value.replace(/,/g, ''));
+  return formatCurrency(number);
+}
