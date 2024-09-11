94829a3a2a597ceb3c4a38b8f7840c41cc60dea4
choycoy <passionfruits479@gmail.com>
2024-09-12 02:11:44 +0900

    파일 이동

---
+++ src/pages/product/components/buttons/CategoryButtons.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import categories from '../../../../data/categories';
+import { useNavigate } from 'react-router-dom';
+
+const CategoryButtons = () => {
+  const navigate = useNavigate();
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedSubCategory, setSelectedSubCategory] = useState(null);
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category === selectedCategory ? null : category);
+    setSelectedSubCategory(null); // Reset subcategory when category changes
+  };
+
+  const handleSubCategoryClick = (subCategory) => {
+    setSelectedSubCategory(subCategory === selectedSubCategory ? null : subCategory);
+  };
+
+  const handleItemClick = (item) => {
+    navigate(`/page/${item}`);
+  };
+
+  const renderItems = () => {
+    if (!selectedCategory) return null;
+
+    const items = categories[selectedCategory];
+    if (!items) return null;
+
+    return (
+      <div className="flex flex-col pl-4 pb-3">
+        <div className="flex flex-wrap gap-2 p-4 max-w-xl">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className="flex-shrink-0 w-1/2 md:w-1/3 lg:w-1/4 cursor-pointer hover:text-custom-green-hover transition-colors whitespace-nowrap hover:scale-105 font-semibold text-lg"
+              onClick={() => handleItemClick(item)}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <div className="flex flex-wrap gap-4 mb-2 max-w-2xl">
+        {Object.keys(categories).map((category) => (
+          <button
+            key={category}
+            className={`px-4 py-2 rounded-lg ${
+              selectedCategory === category
+                ? 'bg-custom-green text-white'
+                : 'bg-custom-gray-light text-custom-black hover:bg-custom-green-hover hover:text-white hover:border-transparent focus:outline-none'
+            }`}
+            onClick={() => handleCategoryClick(category)}
+          >
+            {category.charAt(0).toUpperCase() + category.slice(1)}
+          </button>
+        ))}
+      </div>
+      <div className="inline-flex rounded-lg shadow-lg overflow-hidden bg-custom-green-200">
+        {selectedCategory && renderItems()}
+      </div>
+    </>
+  );
+};
+
+export default CategoryButtons;
