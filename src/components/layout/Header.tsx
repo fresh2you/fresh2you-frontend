3a0958d368425d8a6172ca4e58a6c8f1a3dec732
JIHU96 <korjihu@gmail.com>
2024-09-12 17:08:32 +0900

    fix: 레이아웃 수정
    - MyPage 접속 시 레이아웃의 헤더 적용 X로 수정
    - 하단 메뉴바의 메뉴 색을 디자인 시스템 색으로 변경

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,25 +1,27 @@
-import logoImg from '../../assets/img/logo.png';
-import IconSearch from '../../assets/icons/icon-search.svg';
-import { Link, useLocation } from 'react-router-dom';
-import HeaderInput from './components/HeaderInput';
+import logoImg from "../../assets/img/logo.png";
+import IconSearch from "../../assets/icons/icon-search.svg";
+import { Link, useLocation } from "react-router-dom";
+import HeaderInput from "./components/HeaderInput";
 
 const Header = () => {
   const { pathname } = useLocation();
+
+  if (pathname === "/mypage") return null;
 
   return (
     /* TODO: 테두리 색 디자인 시스템 색상으로 변경 */
     <header className="w-full h-14 px-2 py-2 flex items-center justify-between border-b border-gray-300">
       <h1 className="w-auto h-full flex items-center">
-        <Link to={'/'} className="block h-full">
+        <Link to={"/"} className="block h-full">
           {/* TODO: 가독성으로 인해 로고는 상단바용 로고 별도 제작하기 (텍스트 로고) */}
           <img src={logoImg} alt="fresh to you 로고" className="h-full max-w-full rounded-full" />
           <span className="a11y-hidden">1</span>
         </Link>
       </h1>
 
-      {pathname === '/search' && <HeaderInput />}
+      {pathname === "/search" && <HeaderInput />}
 
-      <Link to={'/search'}>
+      <Link to={"/search"}>
         <IconSearch />
       </Link>
     </header>
