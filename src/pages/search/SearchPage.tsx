093cb777be1707117dc373f37e06c975cdbac70b
JIHU96 <korjihu@gmail.com>
2024-09-13 20:33:28 +0900

    fix: 검색페이지에서 사용하던 제품컴포넌트를 공용컴포넌트로 이동 & 이름 수정

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,27 +1,27 @@
-import { useState } from 'react';
-import useSearchPageLogics from './hooks/useSearchPageLogics';
-import DropDown from '../../components/DropDown';
-import SearchedItemBox from './components/SearchedItemBox';
+import { useState } from "react";
+import useSearchPageLogics from "./hooks/useSearchPageLogics";
+import DropDown from "../../components/DropDown";
+import ProductBox from "../../components/ProductBox";
 
 const SearchPage = () => {
   const { searchedItems } = useSearchPageLogics();
   // TODO: API를 통해 서버로 부터 카테고리 목록 불러오기
-  const mockCategories = ['야채류', '과일류', '해조류'];
-  const [currentCategory, setCurrrentCategory] = useState('');
+  const mockCategories = ["야채류", "과일류", "해조류"];
+  const [currentCategory, setCurrrentCategory] = useState("");
 
   return (
     <>
-      <div className="w-full p-2 relative">
+      <div className="relative w-full p-2">
         <DropDown categories={mockCategories} setCurrrentCategory={setCurrrentCategory} />
 
-        <span className="ml-4 font-bold">{currentCategory || '선택된 카테고리 없음'}</span>
+        <span className="ml-4 font-bold">{currentCategory || "선택된 카테고리 없음"}</span>
       </div>
 
       <section className="px-6 py-2">
-        {searchedItems.length <= 0 && '검색 결과가 없습니다.'}
+        {searchedItems.length <= 0 && "검색 결과가 없습니다."}
 
         {searchedItems.map((item) => (
-          <SearchedItemBox key={item.product_id} item={item} />
+          <ProductBox key={item.product_id} item={item} />
         ))}
       </section>
     </>
