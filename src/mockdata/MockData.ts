b16b9286661e8e6a1bea4831a1b3ce8effd578a3
choycoy <passionfruits479@gmail.com>
2024-09-17 02:45:52 +0900

    이미지 URL로 변경

--- src/mockdata/MockData.ts
+++ src/mockdata/MockData.ts
@@ -1,20 +1,20 @@
-import { ProductCardProps } from '../pages/home/component/ProductCard';
-import itemImg1 from '../assets/img/fruits/Strawberry.png';
-import itemImg2 from '../assets/img/fruits/avocado.png';
-import itemImg3 from '../assets/img/fruits/banana.png';
-import itemImg4 from '../assets/img/fruits/berry.png';
-import itemImg5 from '../assets/img/fruits/blueberry.png';
-import itemImg6 from '../assets/img/fruits/grape.png';
+import { ProductCardProps } from "../pages/home/component/ProductCard";
+const itemImg1 = "https://i.postimg.cc/L8fL37Hc/avocado.png";
+const itemImg2 = "https://i.postimg.cc/jjbNgX2L/banana.png";
+const itemImg3 = "https://i.postimg.cc/5tSzZCQp/berry.png";
+const itemImg4 = "https://i.postimg.cc/V6st1kjw/blueberry.png";
+const itemImg5 = "https://i.postimg.cc/FRPLKXZL/grape.png";
+const itemImg6 = "https://i.postimg.cc/sDb5SyRP/strawberry.png";
 
 const images = [itemImg1, itemImg2, itemImg3, itemImg4, itemImg5, itemImg6];
 
 export const mockProducts: { type: string; products: ProductCardProps[] } = {
-  type: 'user_based',
+  type: "user_based",
   products: Array.from({ length: 1000 }, (_, i) => ({
     product_id: i + 1,
     name: `상품명 ${i + 1}`,
-    seller: '판매자명',
-    description: '상품에 대한 간단 설명입니다',
+    seller: "판매자명",
+    description: "상품에 대한 간단 설명입니다",
     price: 10000,
     img: images[i % images.length],
   })),
