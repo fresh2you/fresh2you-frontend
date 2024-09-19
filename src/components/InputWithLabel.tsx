d03ca2dcd50a4bf16b0f62f1d5de693d106ee34d
JIHU96 <korjihu@gmail.com>
2024-09-19 21:37:12 +0900

    fix: InputWithLabel 공용 컴포넌트의 props 수정
    - type을 옵셔널로 바꾸고 기본값으로 'text' 명시
    - onFocus와 onBlur를 옵셔널로 받음
    - onKeyDown 시 number 타입에 대해서 숫자나 뒤로가기가 아닌 경우 입력을 못하게 설정

--- src/components/InputWithLabel.tsx
+++ src/components/InputWithLabel.tsx
@@ -4,26 +4,40 @@ import { useRef } from "react";
 interface InputWithLabelProps {
   id: string;
   label: string;
-  type: "text" | "password" | "email" | "number";
+  type?: "text" | "password" | "email" | "number";
   value: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   placeholder: string;
   autoComplete?: "on" | "off";
+  onFocus?: () => void;
+  onBlur?: () => void;
 }
 
 const InputWithLabel = ({
   id,
   label,
-  type,
+  type = "text",
   value,
   onChange,
   placeholder,
   autoComplete = "off",
+  onFocus,
+  onBlur,
 }: InputWithLabelProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
+  const onKeyDownCheckNumber = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    if (target.type !== "number") return;
+
+    // 숫자나 뒤로가기가 아닌 입력을 차단
+    if (!/[0-9]/.test(event.key) && event.key !== "Backspace") {
+      event.preventDefault();
+    }
+  };
+
   return (
-    <section className="flex flex-col mb-2">
+    <section className="flex flex-col w-full mb-2">
       <label htmlFor={id} className="mb-1 font-bold" style={{ color: "#333333" }}>
         {label}
       </label>
@@ -36,6 +50,9 @@ const InputWithLabel = ({
           onChange={onChange}
           placeholder={placeholder}
           autoComplete={autoComplete}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onKeyDown={onKeyDownCheckNumber}
           className="w-full text-black outline-none max-h-10"
         />
         {type === "password" && (
