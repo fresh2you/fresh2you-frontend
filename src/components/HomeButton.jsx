be3be3bbb24d07a068e0ce0152847032b62aaad1
choycoy <passionfruits479@gmail.com>
2024-09-20 13:30:19 +0900

    focus-visible, 키보드 포커스시 커스텀 스타일링 커스텀 설정

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -1,16 +1,16 @@
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
+import "../styles/styles.css";
 const HomeButton = ({ className }) => {
   const navigate = useNavigate();
 
   const handleGoHome = () => {
-    navigate('/');
+    navigate("/");
   };
 
   return (
     <button
       onClick={handleGoHome}
-      className={`px-4 py-2 text-base md:text-lg font-semibold rounded-lg ${className} transition hover:border-transparent focus:outline-none`}
+      className={`px-4 py-2 text-base md:text-lg font-semibold rounded-lg ${className} transition hover:border-transparent custom-focus`}
     >
       홈으로 이동
     </button>
