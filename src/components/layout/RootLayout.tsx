899cc8a4734dcfd72383df57342c32da60ad6490
JIHU96 <korjihu@gmail.com>
2024-09-24 17:45:24 +0900

    fix: FNB를 fixed로 수정 & 루트레이아웃 수정

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,12 +1,13 @@
-import { Outlet } from 'react-router-dom';
-import Header from './Header';
-import FooterNavBar from './FooterNavBar';
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import FooterNavBar from "./FooterNavBar";
 
 const RootLayout = () => {
   return (
-    <div className="w-full h-full flex flex-col justify-between">
+    <div className="relative z-0 flex flex-col justify-between w-full h-full">
       <Header />
-      <main className="w-full h-full overflow-hidden overflow-y-scroll scrollbar-hide">
+
+      <main className="pb-[72px] tablet:pb-0 z-20 w-full h-full overflow-y-scroll tablet:px-4 desktop:px-8 ">
         <Outlet />
       </main>
 
