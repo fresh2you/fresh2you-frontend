7db2968384ff2c4de337c272922ec801e4765aba
choycoy <passionfruits479@gmail.com>
2024-09-24 23:01:46 +0900

    카카오 인증 코드 추출 함수

---
+++ src/pages/signIn/hooks/useKakaoLogin.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import useLogin from "./useLogin";
+import { handleKakaoCallback } from "../utils/authUtils";
+
+const useKakaoLogin = (onSuccessCallback, onErrorCallback) => {
+  const mutation = useLogin(true, onSuccessCallback, onErrorCallback);
+
+  useEffect(() => {
+    const code = handleKakaoCallback();
+    if (code) {
+      mutation.mutate({
+        code: code,
+        redirectUri: import.meta.env.VITE_SIGNIN_REDIRECT_URI,
+        provider: "KAKAO",
+        email: "unavailable@naver.com", //test
+      });
+    }
+  }, []);
+
+  return mutation;
+};
+
+export default useKakaoLogin;
--- src/pages/signIn/utils/authUtils.jsx
+++ src/pages/signIn/utils/authUtils.jsx
@@ -1,3 +1,14 @@
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${
   import.meta.env.VITE_KAKAO_API_KEY
 }&redirect_uri=${encodeURIComponent(import.meta.env.VITE_SIGNIN_REDIRECT_URI)}&response_type=code`;
+
+export const handleKakaoCallback = () => {
+  const query = new URLSearchParams(window.location.search);
+  const code = query.get("code");
+
+  if (code) {
+    return code;
+  } else {
+    return null;
+  }
+};
