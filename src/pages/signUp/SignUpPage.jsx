7e028cb17ef84347c891e83383ffdb11a6713f5b
choycoy <passionfruits479@gmail.com>
2024-09-25 03:29:27 +0900

    회원가입 페이지 반응형, api 호출, 응답 철;
    - form 입력시 키보드창 올라와도 불편함 없는 지 확인
    - 회원가입 api 호출
        - 회원가입 api호출하는 동안은 로딩 컴포넌트 표시
        - 이메일 회원가입시
            - form에 입력된 email, pwd, confirmpwd, nickname, state에 저장되어있던 termsAgreements과 provider=”EMAIL”가 담긴 data를 post로 데이터 전달
            - 성공시
                - 로그인 api호출해 **accessToken, acessExpiredAt, prviderId, nickname, provider**가 담긴 데이터를 local storage에 저장
                - 회원 가입 완료 페이지로 이동
            - 실패시 → form reset 시키기를 생각했으나 그대로 두기로 결정

--- src/pages/signUp/SignUpPage.jsx
+++ src/pages/signUp/SignUpPage.jsx
@@ -1,17 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useFunnel } from "@use-funnel/react-router-dom";
 import { getStepsConfig } from "./stepsConfig";
 import { handleSubmit } from "./utils/handlers/handleSubmit";
-import ErrorMessages from "./component/ErrorMessages";
+import ErrorMessages from "./component/errormsg/ErrorMessages";
 import PasswordFeedback from "./component/PasswordFeedback";
 import StepFields from "./component/StepFields";
-import { validationLogic } from "./utils/validationLogic";
-import { handleConfirmPasswordChange } from "./utils/passwordUtils";
 import SubmitBtn from "./component/buttons/SubmitBtn";
 import { useResetStatusOnEmailChange, useResetStatusOnNicknameChange } from "./hooks/useResetStatusOnFieldChange";
 import EmailVerificationModal from "./component/EmailVerificationModal";
-
+import { usePasswordValidation } from "./hooks/usePasswordValidation";
+import { useFormValidation } from "./hooks/useFormValidation";
+import { useNavigate } from "react-router-dom";
+import ErrorModal from "./component/ErrorModal";
+import { useLocation } from "react-router-dom";
+import { Loading } from "../redirection/component/Loading";
 export default function SignUpPage() {
+  const navigate = useNavigate();
+  const location = useLocation();
   const funnel = useFunnel({
     id: "user-signup",
     initial: {
@@ -24,70 +29,95 @@ export default function SignUpPage() {
       },
     },
   });
+
   const [formData, setFormData] = useState({
     email: funnel.context.email,
     password: funnel.context.password,
     confirmPassword: funnel.context.confirmPassword,
     nickname: funnel.context.nickname,
   });
+
   const [status, setStatus] = useState({
     emailStatus: "",
     passwordStatus: "",
     nicknameStatus: "",
   });
+
   const [validity, setValidity] = useState({
     isEmailValid: false,
     isPasswordCombinationValid: false,
     isConfirmPasswordValid: false,
     isNicknameValid: false,
   });
-  const [passwordFeedbacks, setPasswordFeedbacks] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [verificationCode, setVerificationCode] = useState("");
 
-  const handleOpenModal = () => setIsModalOpen(true);
-  const handleCloseModal = () => setIsModalOpen(false);
+  const [isErrorModalOpen, setIsErrorModalOpen] = useState(false);
+  const [errorModalMessage, setErrorModalMessage] = useState("");
+
+  const [isVerificationModalOpen, setIsVerificationModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleOpenErrorModal = () => setIsErrorModalOpen(true);
+  const handleCloseErrorModal = () => setIsErrorModalOpen(false);
+
+  const handleOpenVerificationModal = () => setIsVerificationModalOpen(true);
+  const handleCloseVerificationModal = () => setIsVerificationModalOpen(false);
 
   useResetStatusOnEmailChange(formData.email, setStatus);
   useResetStatusOnNicknameChange(formData.nickname, setStatus);
+  useFormValidation(formData, status, setValidity, funnel);
 
-  useEffect(() => {
-    validationLogic.updateValidity(formData, status, setValidity, funnel);
-  }, [status, formData]);
-
-  useEffect(() => {
-    const passwordFeedbackMessages = validationLogic.getPasswordFeedbackMessages(formData.password);
-    setPasswordFeedbacks(passwordFeedbackMessages);
-    if (formData.confirmPassword) {
-      handleConfirmPasswordChange(formData.confirmPassword, formData.password, setFormData, setStatus);
-    }
-  }, [formData.password, formData.confirmPassword]);
-  useEffect(() => {
-    console.log(status.emailStatus);
-  }, [status.emailStatus]);
-
-  const steps = getStepsConfig(formData, setFormData, setStatus, validity, handleOpenModal);
+  const { passwordFeedbacks } = usePasswordValidation(formData, setFormData, setStatus);
+  const steps = getStepsConfig(formData, setFormData, setStatus, validity, handleOpenVerificationModal);
   const currentStep = funnel.step;
-  console.log(validity);
+
+  const handleFormSubmit = (e) => {
+    handleSubmit(
+      e,
+      validity,
+      formData,
+      navigate,
+      setErrorModalMessage,
+      handleOpenErrorModal,
+      location.state,
+      funnel,
+      setFormData,
+      setStatus,
+      setValidity,
+      setIsLoading,
+    );
+  };
+
   return (
     <>
-      <form
-        onSubmit={(e) => handleSubmit(e, validity, formData)}
-        className="flex flex-col justify-center min-h-screen my-0 mx-auto w-[290px] items-start"
-      >
-        <PasswordFeedback passwordFeedbacks={passwordFeedbacks} />
-        <ErrorMessages status={status} />
-        <StepFields steps={steps} currentStep={currentStep} validity={validity} funnel={funnel} formData={formData} />
-        {currentStep === "닉네임입력" && <SubmitBtn validity={validity} />}
-      </form>
-      {isModalOpen && (
-        <EmailVerificationModal
-          onClose={handleCloseModal}
-          email={formData.email}
-          setStatus={setStatus}
-          handleCloseModal={handleCloseModal}
-          isEmailValid={validity.isEmailValid}
-        />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <form
+            onSubmit={handleFormSubmit}
+            className="flex flex-col justify-center min-h-screen mx-auto w-[290px] items-start py-8"
+          >
+            <PasswordFeedback passwordFeedbacks={passwordFeedbacks} />
+            <ErrorMessages status={status} />
+            <StepFields
+              steps={steps}
+              currentStep={currentStep}
+              validity={validity}
+              funnel={funnel}
+              formData={formData}
+            />
+            {currentStep === "닉네임입력" && <SubmitBtn validity={validity} />}
+          </form>
+          {isErrorModalOpen && <ErrorModal message={errorModalMessage} onClose={handleCloseErrorModal} />}
+          {isVerificationModalOpen && (
+            <EmailVerificationModal
+              onClose={handleCloseVerificationModal}
+              email={formData.email}
+              setStatus={setStatus}
+              isEmailValid={validity.isEmailValid}
+            />
+          )}
+        </>
       )}
     </>
   );
