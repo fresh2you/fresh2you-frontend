a1bef3abe395193f2151c66d3a2ac3d71448e067
JIHU96 <korjihu@gmail.com>
2024-09-24 18:38:16 +0900

    fix: 상단 헤더의 검색 아이콘 조건부 렌더링 위치 변경
    - HeaderRight -> Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,12 +3,13 @@ import { Link, useLocation } from "react-router-dom";
 import HeaderInput from "./components/HeaderInput";
 import HeaderRight from "@/components/layout/components/HeaderRight";
 import HeaderCenterMenus from "@/components/layout/components/HeaderCenterMenus";
+import IconSearch from "@/assets/icons/icon-search.svg";
 
 const Header = () => {
   const { pathname } = useLocation();
 
   return (
-    <header className="flex items-center justify-between w-full gap-2 px-2 py-1 border-gray-300 h-14 tablet-sm:px-4 tablet-sm:gap-4 desktop-sm:gap-8">
+    <header className="flex items-center justify-between w-full gap-2 px-4 py-1 border-gray-300 h-14 tablet-sm:px-4 tablet-sm:gap-4 ">
       <h1 className="flex items-center h-4/5 tablet:h-full">
         <Link to={"/"} className="h-full">
           <TextLogo className="w-auto h-full aspect-auto text-custom-green" />
@@ -19,7 +20,13 @@ const Header = () => {
       {/* desktop 이상일 경우에만 렌더링 */}
       <HeaderCenterMenus />
 
-      {pathname === "/search" && <HeaderInput />}
+      {pathname === "/search" ? (
+        <HeaderInput />
+      ) : (
+        <Link to={"/search"} className="ml-auto">
+          <IconSearch className="w-6 h-6 text-black" />
+        </Link>
+      )}
 
       <HeaderRight />
     </header>
--- src/components/layout/components/HeaderRight.tsx
+++ src/components/layout/components/HeaderRight.tsx
@@ -1,20 +1,10 @@
-import IconSearch from "@/assets/icons/icon-search.svg";
-import { Link, useLocation } from "react-router-dom";
 import MenuDropDown from "@/components/layout/components/MenuDropDown";
 import { headerDropdownMenus } from "@/components/layout/constants/menus";
 import HeaderUserInfo from "@/components/layout/components/HeaderUserInfo";
 
 const HeaderRight = () => {
-  const { pathname } = useLocation();
-
   return (
-    <div className="items-center hidden h-full gap-2 tablet:flex">
-      {pathname !== "/search" && (
-        <Link to={"/search"}>
-          <IconSearch className="w-6 h-6 text-black" />
-        </Link>
-      )}
-
+    <div className="items-center hidden h-full gap-4 tablet:flex">
       <HeaderUserInfo />
 
       <div className="hidden tablet:block desktop:hidden">
