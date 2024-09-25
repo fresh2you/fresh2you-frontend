8ddacf9837cd3b5d0af1fdaea39ced84353b7696
choycoy <passionfruits479@gmail.com>
2024-09-25 21:48:00 +0900

    인증 코드 추출 함수 useParams로 수정

--- src/pages/signIn/utils/authUtils.jsx
+++ src/pages/signIn/utils/authUtils.jsx
@@ -1,14 +1,9 @@
+import { useParams } from "react-router-dom";
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${
   import.meta.env.VITE_KAKAO_API_KEY
 }&redirect_uri=${encodeURIComponent(import.meta.env.VITE_SIGNIN_REDIRECT_URI)}&response_type=code`;
 
 export const handleKakaoCallback = () => {
-  const query = new URLSearchParams(window.location.search);
-  const code = query.get("code");
-
-  if (code) {
-    return code;
-  } else {
-    return null;
-  }
+  const { code } = useParams();
+  return code;
 };
