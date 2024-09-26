b1c99b6d661e505bd000e9a6a8d7be0af8cb8b99
choycoy <passionfruits479@gmail.com>
2024-09-27 00:34:12 +0900

    회원가입 페이지, 자동 스크롤이 아닌 다음 버튼으로 변경

--- src/pages/signUp/SignUpComplete.jsx
+++ src/pages/signUp/SignUpComplete.jsx
@@ -6,14 +6,17 @@ import slider1 from "../../assets/img/sliders/slider1.png";
 import slider2 from "../../assets/img/sliders/slider2.png";
 import slider3 from "../../assets/img/sliders/slider3.png";
 import HomeButton from "../../components/HomeButton";
+import { useRef } from "react";
+import { useState } from "react";
+import "../../styles/styles.css";
 const settings = {
   dots: true,
-  infinite: true,
+  infinite: false,
   speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
-  autoplay: true,
-  arrows: true,
+  autoplay: false,
+  arrows: false,
 };
 
 const Slide = ({ title, description, imgSrc, imgAlt }) => (
@@ -28,15 +31,27 @@ const Slide = ({ title, description, imgSrc, imgAlt }) => (
 
 const SignUpCompletePage = () => {
   const navigate = useNavigate();
+  const sliderRef = useRef(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   const handleGoHome = () => {
     navigate("/");
   };
 
+  const handleNextSlide = () => {
+    if (sliderRef.current) {
+      sliderRef.current.slickNext();
+    }
+  };
+
+  const handleBeforeChange = (current, next) => {
+    setCurrentSlide(next);
+  };
+
   return (
     <div className="landing-page flex flex-col justify-center items-center h-screen px-4 md:px-8">
       <div className="w-full max-w-5xl h-auto">
-        <Slider {...settings}>
+        <Slider {...settings} ref={sliderRef} beforeChange={handleBeforeChange}>
           <Slide
             title="신선한 농산물을 직접 만나요!"
             description="중간 유통과정을 생략하고, 농부와 소비자가 직접 거래하는 시스템이에요"
@@ -57,7 +72,19 @@ const SignUpCompletePage = () => {
           />
         </Slider>
       </div>
-      <HomeButton className="mt-10 bg-custom-green hover:bg-custom-green-hover text-white" />
+      <div className="flex space-x-4 mt-10">
+        {currentSlide === 2 ? (
+          <HomeButton className="bg-custom-green hover:bg-custom-green-hover text-white" onClick={handleGoHome} />
+        ) : (
+          <button
+            className="bg-custom-green hover:bg-custom-green-hover text-white px-6 rounded-lg 
+          font-semibold text-custom-btn-text py-2 tablet:py-1.5 custom-focus"
+            onClick={handleNextSlide}
+          >
+            다음
+          </button>
+        )}
+      </div>
     </div>
   );
 };
