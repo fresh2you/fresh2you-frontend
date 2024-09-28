35c5310314f66744497d75b23a34d59ed3e477ab
JIHU96 <korjihu@gmail.com>
2024-09-28 18:14:53 +0900

    fix: 마이페이지 전용 레이아웃을 전체 페이지 레이아웃으로 적용

--- src/pages/mypage/components/PageLayout.tsx
+++ src/pages/mypage/components/PageLayout.tsx
@@ -1,15 +1,15 @@
 import { Outlet } from "react-router-dom";
 import CommonHeader from "@/components/CommonHeader";
 
-const MyPageLayout = () => {
+const PageLayout = () => {
   return (
     <div className="flex flex-col w-full h-full">
       <CommonHeader />
-      <main className="w-full h-full overflow-hidden overflow-y-scroll scrollbar-hide">
+      <main className="flex justify-center w-full h-full overflow-hidden overflow-y-scroll scrollbar-hide mobile:px-4 desktop:px-10 max-w-7xl">
         <Outlet />
       </main>
     </div>
   );
 };
 
-export default MyPageLayout;
+export default PageLayout;
