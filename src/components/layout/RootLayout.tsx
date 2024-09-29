7e3212fa15fa7417a7e0e2e36e2e0c5ecade76d9
choycoy <passionfruits479@gmail.com>
2024-09-29 17:54:27 +0900

    Reapply "Reapply "Merge branch 'main' into refactor/products""
    This reverts commit a8a6097ac9c167b90d582839a4d5ec4edab3a320.

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,13 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header";
 import FooterNavBar from "./FooterNavBar";
 
 const RootLayout = () => {
+  const { pathname } = useLocation();
+  const validPaths = ["/", "/community", "/product", "/chatting", "/mypage", "/search"];
+
   return (
     <div className="relative z-0 flex flex-col justify-between w-full h-full">
       <Header />
 
-      <main className="pb-[72px] tablet:pb-0 z-20 w-full h-full overflow-y-scroll tablet:px-4 desktop:px-8 ">
+      <main
+        className={`${
+          validPaths.includes(pathname) ? "pb-[72px]" : ""
+        } tablet:pb-0 z-20 w-full h-full overflow-y-scroll tablet:px-4 desktop:px-8 `}
+      >
         <Outlet />
       </main>
 
