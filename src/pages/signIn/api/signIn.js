3b2eae4cbf97028fd184193a6a2edfb9666f8ceb
JIHU96 <korjihu@gmail.com>
2024-10-02 00:01:01 +0900

    fix: login 함수 수정
    - instance로 바로 사용하는 것이 아니라 api 리스트에서 가져와 사용으로 변경
    - localStorage에 토큰 저장하는 코드 삭제 => 다른 로직으로 이동

--- src/pages/signIn/api/signIn.js
+++ src/pages/signIn/api/signIn.js
@@ -1,14 +1,14 @@
-import { instance } from "@/instance";
+import { api } from "@/services/api";
 
 export const login = async (credentials) => {
-  const response = await instance.post("/members/login", credentials);
+  try {
+    const { data: response } = await api.auth.emailLogin({
+      email: credentials.email,
+      password: credentials.password,
+    });
 
-  if (response.data.success) {
-    const { accessToken, accessExpiredAt } = response.data.data.token;
-
-    localStorage.setItem("accessToken", accessToken);
-    localStorage.setItem("accessExpiredAt", accessExpiredAt);
-  } else {
+    return response;
+  } catch (error) {
     throw new Error("로그인 정보가 일치하지 않습니다.");
   }
 };
