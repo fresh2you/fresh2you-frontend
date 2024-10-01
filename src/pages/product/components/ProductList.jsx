10a46fc6d0bedc47b8f01b4b2185118e288c2619
choycoy <passionfruits479@gmail.com>
2024-10-02 04:30:19 +0900

    Design: skeleton UI 첫 번째 렌더링 시에만 Skeleton을 표시

--- src/pages/product/components/ProductList.jsx
+++ src/pages/product/components/ProductList.jsx
@@ -1,20 +1,23 @@
+import { useState, useEffect } from "react";
 import ProductCard from "./ProductCard";
 import ProductCardSkeleton from "./skeletons/ProductCardSkeleton";
 
 const ProductList = ({ products, lastProductRef, itemsPerPage }) => {
+  const [isInitialRender, setIsInitialRender] = useState(true);
+
+  useEffect(() => {
+    setIsInitialRender(false);
+  }, []);
+
   return (
-    <div
-      className="grid grid-cols-2 tablet:grid-cols-3 desktop-sm:grid-cols-4 mobile:gap-y-3
-          tablet:gap-y-6 tablet:gap-x-3 justify-items-center"
-    >
-      {products.map((product, index) => {
-        const key = `${product.product_id}-${index}`;
-        return (
-          <div ref={index === products.length - 1 ? lastProductRef : null} key={key}>
-            <ProductCard product={product} />
-          </div>
-        );
-      })}
+    <div className="grid grid-cols-2 tablet:grid-cols-3 desktop-sm:grid-cols-4 mobile:gap-y-3 tablet:gap-y-6 tablet:gap-x-3 justify-items-center">
+      {isInitialRender
+        ? Array.from({ length: itemsPerPage }, (_, index) => <ProductCardSkeleton key={index} />)
+        : products.map((product, index) => (
+            <div ref={index === products.length - 1 ? lastProductRef : null} key={`${product.product_id}-${index}`}>
+              <ProductCard product={product} />
+            </div>
+          ))}
     </div>
   );
 };
