48fa22fc8ad6ded8d17207f425d95dc1260da59c
choycoy <passionfruits479@gmail.com>
2024-10-02 19:46:16 +0900

    FEAT: 판매자일시 상품 등록하기 버튼 활성화

--- src/pages/product/ProductsPage.jsx
+++ src/pages/product/ProductsPage.jsx
@@ -6,7 +6,11 @@ import useInfiniteScroll from "./hooks/useInfiniteScroll";
 import ProductList from "./components/ProductList";
 import "../../styles/styles.css";
 import { fetchProducts } from "./api/productApis";
+import { useQueryClient } from "@tanstack/react-query";
+import PlusIcon from "../../assets/icons/plus.svg";
+import { useNavigate } from "react-router-dom";
 const ProductsPage = () => {
+  const queryClient = useQueryClient();
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [pageNumber, setPageNumber] = useState(0);
@@ -14,6 +18,7 @@ const ProductsPage = () => {
   const [selectedCategoryId, setSelectedCategoryId] = useState(undefined);
   const itemsPerPage = 12;
 
+  const navigate = useNavigate();
   const observer = useRef();
 
   const loadProducts = useCallback(
@@ -49,11 +54,23 @@ const ProductsPage = () => {
     loadProducts(pageNumber);
   }, [pageNumber, loadProducts]);
 
+  const userInfo = queryClient.getQueryData("userInfo"); //need to check
+  const isSeller = userInfo ? userInfo.isSeller : false;
+
   return (
     <div className="mx-auto py-2.5 text-custom-black product-page">
       <CategoryButtons handleCategoryChange={handleCategoryChange} />
-      <h2 className="font-bold text-center mb-6 text-custom-green mt-4 text-custom-h2">갓 수확했어요!</h2>
+      <h2 className="font-bold text-center text-custom-green my-6 text-custom-h2">갓 수확했어요!</h2>
       <ProductList products={products} lastProductRef={lastProductRef} itemsPerPage={itemsPerPage} />
+      {isSeller && (
+        <button
+          className="fixed mobile:bottom-20 tablet:bottom-4 mobile:right-4 p-2.5 bg-custom-green text-white rounded-full 
+      shadow-lg hover:bg-custom-green-hover z-50"
+          onClick={() => navigate("/product/register")}
+        >
+          <PlusIcon className="mobile:w-7 mobile:h-7 tablet:w-8 tablet:h-8" />
+        </button>
+      )}
     </div>
   );
 };
