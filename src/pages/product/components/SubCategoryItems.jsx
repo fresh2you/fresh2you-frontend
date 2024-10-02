5fef7095b4f465a78cd8f60f679d1fe43633d0b6
choycoy <passionfruits479@gmail.com>
2024-10-02 18:33:12 +0900

    FIX: 2차 카테고리 버튼 클릭시 /product/category_id 로 경로이동 제거

--- src/pages/product/components/SubCategoryItems.jsx
+++ src/pages/product/components/SubCategoryItems.jsx
@@ -1,13 +1,16 @@
+import "../../../styles/styles.css";
 const SubCategoryItems = ({ items, onItemClick }) => {
   return (
-    <div className="flex flex-col pl-1.5 py-1">
+    <div
+      className="flex flex-col pl-1.5 py-1 absolute z-10 bg-custom-green-200 
+    category-modal rounded-lg shadow-lg mt-2 left-1/2 -translate-x-1/2"
+    >
       <div className="flex flex-wrap gap-2 p-2">
         {items.map((item) => (
           <div
             key={item.categoryId}
             className="flex-shrink-0 w-1/4 cursor-pointer hover:text-custom-green-hover transition-colors 
               hover:scale-105 font-semibold text-sm mr-4 whitespace-nowrap"
-            onClick={() => onItemClick(item.categoryId)}
           >
             {item.categoryName}
           </div>
