a174f1026deb0fb97d864f3d7da4a4defa3a96c7
choycoy <passionfruits479@gmail.com>
2024-10-03 13:41:49 +0900

    FIX: 채팅 메시지 응답형식에 맞게 변경

--- src/pages/chat/components/MessageItem.jsx
+++ src/pages/chat/components/MessageItem.jsx
@@ -1,16 +1,22 @@
-const MessageItem = ({ message }) => {
-  const userId = localStorage.getItem("id");
+const MessageItem = ({ message, userId }) => {
+  const formatTimestamp = (timestamp) => {
+    const date = new Date(timestamp);
+    return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+  };
+  const isSender = message.senderId === userId;
   return (
-    <div className={`flex items-end py-2 ${message.sender === "고객" ? "self-end pr-2" : "self-start pl-2"} w-3/5`}>
-      <div className={`flex ${message.receiver === "고객" ? "justify-start" : "justify-end"} w-full`}>
+    <div className={`flex items-end py-2 ${isSender ? "self-start pl-2" : "self-end pr-2 "} max-w-3/5`}>
+      {!isSender && <span className={`text-xs text-gray-500 mr-2 self-end`}>{formatTimestamp(message.timestamp)}</span>}
+      <div className={`flex ${isSender ? "justify-start" : "justify-end"}`}>
         <p
           className={`py-2 px-3 rounded-lg leading-5 break-words ${
-            message.sender === "고객" ? "bg-custom-green text-white" : "bg-gray-100"
+            isSender ? "bg-gray-100 " : "bg-custom-green text-white"
           }`}
         >
-          {message.text}
+          {message.content}
         </p>
       </div>
+      {isSender && <span className={`text-xs text-gray-500 ml-2`}>{formatTimestamp(message.timestamp)}</span>}
     </div>
   );
 };
