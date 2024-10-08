c789609f63b4332c754c59faad1023bd5d6b42db
choycoy <passionfruits479@gmail.com>
2024-10-08 15:11:53 +0900

    FIX: 카카오 로그인 api 호출 tsx -> jsx로 변경

--- src/pages/signIn/hooks/useKakaoLogin.ts
+++ src/pages/signIn/hooks/useKakaoLogin.ts
@@ -2,10 +2,14 @@ import { useEffect } from "react";
 import useLogin from "./useLogin";
 import { handleKakaoCallback } from "../utils/authUtils";
 
-const useKakaoLogin = (onSuccessCallback, onErrorCallback) => {
+interface UseKakaoLoginProps {
+  onSuccessCallback: (data: ISocialLoginResponse["data"] | null) => void;
+  onErrorCallback: () => void;
+}
+const useKakaoLogin = ({ onSuccessCallback, onErrorCallback }: UseKakaoLoginProps) => {
   const mutation = useLogin(true, onSuccessCallback, onErrorCallback);
 
-  const redirectUri =
+  const redirectUri: string =
     import.meta.env.MODE === "development"
       ? import.meta.env.VITE_SIGNIN_REDIRECT_URI
       : import.meta.env.VITE_PROD_SIGNIN_REDIRECT_URI;
--- src/pages/signIn/utils/authUtils.ts
+++ src/pages/signIn/utils/authUtils.ts
@@ -1,8 +1,3 @@
-const redirectUri =
-  import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_SIGNIN_REDIRECT_URI
-    : import.meta.env.VITE_PROD_SIGNIN_REDIRECT_URI;
-
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${
   import.meta.env.VITE_KAKAO_API_KEY
 }&redirect_uri=${encodeURIComponent(import.meta.env.VITE_SIGNIN_REDIRECT_URI)}&response_type=code`;
