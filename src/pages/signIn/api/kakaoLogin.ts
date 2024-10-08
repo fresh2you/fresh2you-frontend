b1f510fadac3b4cd79e177a2adabc74a0e931425
choycoy <passionfruits479@gmail.com>
2024-10-08 15:07:32 +0900

    FIX: 카카오 로그인 api jsx -> tsx로 수정

---
+++ src/pages/signIn/api/kakaoLogin.ts
@@ -0,0 +1,30 @@
+import { api } from "@/services/api";
+interface KakaoLoginParams {
+  code: string;
+  redirectUri: string;
+  provider: "KAKAO";
+}
+
+export const kakaoLogin = async (accessToken: KakaoLoginParams): Promise<ISocialLoginResponse> => {
+  try {
+    const response = await api.auth.socialLogin(accessToken);
+    const { token, loginMember, isSignup } = response.data;
+
+    if (response.success) {
+      localStorage.setItem("accessToken", token.accessToken);
+      localStorage.setItem("accessExpiredAt", token.accessExpiredAt);
+    }
+    return {
+      success: response.success,
+      code: response.code,
+      message: response.message,
+      data: {
+        token: token,
+        loginMember: loginMember,
+        isSignup: isSignup,
+      },
+    };
+  } catch {
+    throw new Error("카카오 로그인에 실패했습니다.");
+  }
+};
