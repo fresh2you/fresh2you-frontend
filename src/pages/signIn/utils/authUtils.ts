f04bc158a55e45ee631bde8e5c09980a96d3d0da
JIHU96 <korjihu@gmail.com>
2024-10-11 02:48:18 +0900

    refactor: 카카오 소셜로그인의 redirectURI를 실행 환경에 따라 분기

--- src/pages/signIn/utils/authUtils.ts
+++ src/pages/signIn/utils/authUtils.ts
@@ -1,6 +1,10 @@
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${
   import.meta.env.VITE_KAKAO_API_KEY
-}&redirect_uri=${encodeURIComponent(import.meta.env.VITE_SIGNIN_REDIRECT_URI)}&response_type=code`;
+}&redirect_uri=${encodeURIComponent(
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_SIGNIN_REDIRECT_URI
+    : import.meta.env.VITE_PROD_SIGNIN_REDIRECT_URI,
+)}&response_type=code`;
 
 export const handleKakaoCallback = () => {
   const urlParams = new URLSearchParams(window.location.search);
