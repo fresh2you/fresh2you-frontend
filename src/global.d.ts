70589ac96be5e01e2d24303d03c173eec2ce6141
JIHU96 <korjihu@gmail.com>
2024-10-16 02:22:20 +0900

    chore: SVG 컴포넌트에 옵셔널로 alt 속성 타입 추가

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,7 +2,7 @@
 declare module "*.svg" {
   import React from "react";
   const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { className?: string }
+    React.SVGProps<SVGSVGElement> & { className?: string } & { alt?: string }
   >;
   export default ReactComponent;
 }
