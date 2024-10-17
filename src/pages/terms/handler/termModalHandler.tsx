69c5f199ecbbcc5330d563b305d56c9d7f3c4758
choycoy <passionfruits479@gmail.com>
2024-10-18 01:12:24 +0900

    FIX: 특수문자 escape하기

--- src/pages/terms/handler/termModalHandler.tsx
+++ src/pages/terms/handler/termModalHandler.tsx
@@ -1,8 +1,14 @@
 import { overlay } from "overlay-kit";
 import TermModal from "../components/TermModal";
 
+const escapeString = (str: string) => {
+  return str.replace(/\\/g, "\\\\").replace(/"/g, '\\"').replace(/\n/g, "\\n").replace(/\r/g, "\\r");
+};
+
 export const openTermModal = (content: string) => {
-  const unescapedData = JSON.parse(`"${content}"`);
+  const escapedContent = escapeString(content);
+  const unescapedData = JSON.parse(`"${escapedContent}"`);
+
   overlay.open(({ isOpen, close }) => {
     return <TermModal isOpen={isOpen} onClose={close} content={unescapedData} />;
   });
