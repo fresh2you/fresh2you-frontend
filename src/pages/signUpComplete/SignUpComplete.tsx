639b6b55f4ee16ee7c172e5d26c2b04701996f42
choycoy <passionfruits479@gmail.com>
2024-10-17 19:57:40 +0900

    FIX: 회원가입 완료 페이지, 이미지 svg로 변경및 시맨틱 태그 적용

--- src/pages/signUpComplete/SignUpComplete.tsx
+++ src/pages/signUpComplete/SignUpComplete.tsx
@@ -1,9 +1,9 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import slider1 from "../../assets/img/sliders/slider1.png";
-import slider2 from "../../assets/img/sliders/slider2.png";
-import slider3 from "../../assets/img/sliders/slider3.png";
+import Slider1 from "../../assets/img/sliders/slider1.svg";
+import Slider2 from "../../assets/img/sliders/slider2.svg";
+import Slider3 from "../../assets/img/sliders/slider3.svg";
 import { useRef, useState } from "react";
 import Slide from "./component/Slide";
 import SliderControls from "./component/SliderControls";
@@ -40,36 +40,33 @@ const SignUpCompletePage = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen px-4 mobile:w-full mobile:max-w-[420px] mx-auto tablet-sm:w-11/12 tablet-sm:max-w-[650px] desktop-sm:max-w-[800px]">
-      <main className="w-full h-auto">
+    <main className="flex flex-col justify-center items-center h-screen px-4 mobile:w-full mobile:max-w-[420px] mx-auto tablet-sm:w-11/12 tablet-sm:max-w-[650px] desktop-sm:max-w-[800px]">
+      <section className="w-full h-auto">
         <Slider {...settings} ref={sliderRef} beforeChange={handleBeforeChange}>
           <Slide
             title="신선한 농산물을 직접 만나요!"
             description="중간 유통과정을 생략하고, 농부와 소비자가 직접 거래하는 시스템이에요"
-            imgSrc={slider1}
-            imgAlt="농부와 소비자"
+            imgSrc={<Slider1 alt="농부와 소비자" className="h-1/2 w-1/2 max-w-64" />}
           />
           <Slide
             title="실시간으로 직접 협상해요!"
             description="농부와 직접 채팅하며 상품 상태와 가격을 협상할 수 있어요"
-            imgSrc={slider2}
-            imgAlt="직접 협상"
+            imgSrc={<Slider2 alt="직접 협상" className="h-1/2 w-1/2 max-w-64" />}
           />
           <Slide
             title="믿을 수 있는 판매자만!"
             description="인증을 받은 판매자만이 상품을 판매할 수 있습니다. 안전하고 신뢰할 수 있는 거래를 보장해요"
-            imgSrc={slider3}
-            imgAlt="판매자 인증"
+            imgSrc={<Slider3 alt="판매자 인증" className="h-1/2 w-1/2 max-w-64" />}
           />
         </Slider>
-      </main>
+      </section>
       <SliderControls
         currentSlide={currentSlide}
         onNextSlide={handleNextSlide}
         onPreviousSlide={handlePreviousSlide}
         totalSlides={totalSlides}
       />
-    </div>
+    </main>
   );
 };
 
--- src/pages/signUpComplete/component/Slide.tsx
+++ src/pages/signUpComplete/component/Slide.tsx
@@ -3,17 +3,16 @@ import React from "react";
 interface SlideProps {
   title: string;
   description: string;
-  imgSrc: string;
-  imgAlt: string;
+  imgSrc: JSX.Element;
 }
 
-const Slide: React.FC<SlideProps> = ({ title, description, imgSrc, imgAlt }) => (
+const Slide: React.FC<SlideProps> = ({ title, description, imgSrc }) => (
   <div className="slide flex flex-col justify-center items-center desktop-sm:flex-row desktop-sm:items-start desktop-sm:gap-x-2">
     <div className="flex flex-col mb-1 desktop-sm:w-96">
       <h1 className="font-semibold text-custom-green text-custom-h2 whitespace-nowrap">{title}</h1>
       <p className="mt-1 text-custom-p">{description}</p>
     </div>
-    <img src={imgSrc} alt={imgAlt} className="h-1/2 w-1/2 max-w-64" />
+    {imgSrc}
   </div>
 );
 
