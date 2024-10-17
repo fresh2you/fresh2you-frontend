1b6035f41593067f2f949678762fab9e4c9fa29c
choycoy <passionfruits479@gmail.com>
2024-10-17 19:37:09 +0900

    FIX: 404 페이지 시맨틱 태그 적용

--- src/pages/NotfoundPage.tsx
+++ src/pages/NotfoundPage.tsx
@@ -3,8 +3,8 @@ import HomeButton from "@/components/HomeButton";
 
 const NotFoundPage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen px-4 bg-custom-green text-custom-black">
-      <div className="flex items-center justify-center w-11/12 gap-x-4">
+    <main className="flex flex-col items-center justify-center h-screen px-4 bg-custom-green text-custom-black">
+      <section className="flex items-center justify-center w-11/12 gap-x-4">
         <div className="w-1/3 max-w-[180px]">
           <LogoImg aria-label="Fresh 2 You" className="h-auto" />
         </div>
@@ -12,9 +12,9 @@ const NotFoundPage: React.FC = () => {
           <h1 className="font-semibold text-white text-custom-h">404</h1>
           <p className="font-semibold text-custom-p">페이지를 찾을 수 없습니다.</p>
         </div>
-      </div>
+      </section>
       <HomeButton className="mt-1.5 bg-white text-custom-black hover:bg-custom-gray-light" />
-    </div>
+    </main>
   );
 };
 
