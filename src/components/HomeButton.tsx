2627b69cac1da05b0fa7e3107b2b31b714569777
choycoy <passionfruits479@gmail.com>
2024-10-17 19:58:12 +0900

    FIX: 홈 버튼, button tag에서 LInk태그로 변경

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -1,25 +1,19 @@
-import { useNavigate } from "react-router-dom";
 import "../styles/styles.css";
+import { Link } from "react-router-dom";
 
 interface HomeButtonProps {
   className?: string;
 }
 
 const HomeButton: React.FC<HomeButtonProps> = ({ className = "" }) => {
-  const navigate = useNavigate();
-
-  const handleGoHome = () => {
-    navigate("/");
-  };
-
   return (
-    <button
-      onClick={handleGoHome}
+    <Link
+      to={"/"}
       className={`px-2 py-1.5 font-semibold rounded-lg ${className} transition 
       custom-focus text-custom-btn-text`}
     >
       홈으로 이동
-    </button>
+    </Link>
   );
 };
 
