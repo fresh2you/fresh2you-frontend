b39cab581036357c41894e88a6998e4485c542b6
choycoy <passionfruits479@gmail.com>
2024-10-25 02:35:14 +0900

    FIX: api 구조 개선

--- src/pages/product/api/productApis.js
+++ src/pages/product/api/productApis.js
@@ -1,49 +1,6 @@
 import { instance } from "@/instance";
 const token = localStorage.getItem("accessToken");
 
-export const fetchCategories = async () => {
-  try {
-    const response = await instance.get("/categories", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return response.data.data.categories;
-  } catch (error) {
-    console.error("Error fetching categories:", error);
-    throw error;
-  }
-};
-
-export const fetchProducts = async (categoryId = "", keyword = "", pageNumber = 0, itemsPerPage = 3) => {
-  try {
-    const params = {
-      page: pageNumber,
-      size: itemsPerPage,
-    };
-
-    if (categoryId) {
-      params.categoryId = categoryId;
-    }
-
-    if (keyword) {
-      params.keyword = keyword;
-    }
-
-    const response = await instance.get("/products", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      params,
-    });
-
-    return response.data.data;
-  } catch (error) {
-    console.error("Error fetching products:", error);
-    throw error;
-  }
-};
-
 export const fetchProductById = async (productId) => {
   try {
     const response = await instance.get(`/products/${productId}`, {
