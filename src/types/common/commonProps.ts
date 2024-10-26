646123774b87ea446497fd414da3d985510cb4d6
choycoy <passionfruits479@gmail.com>
2024-10-26 18:21:49 +0900

    REFACTOR: 공용 컴포넌트 리팩토링
    - 비지니스 로직과 UI로직 분리
    - 속성 값 interface따로 정의
    -  SEO 개선을 위해 ARIA- 속성 추가
      - 비밀번호 토글 버튼에 aria-pressed와 aria-label 속성을 추가하여 접근성을 높였습니다.
      - aria-live 속성을 추가하여 길이 제한 메시지가 필요한 경우에만 사용자에게 알려지도록 했습니다.
      - 접근성 도구에서 필드가 필수임을 알리기 위해 input tag에 aria-required 속성과 aria-labelledby 속성을 사용하여 스크린 리더가 해당 입력 필드에 대한 레이블을 찾을 수 있게 도와주게 하였습니다.

---
+++ src/types/common/commonProps.ts
@@ -0,0 +1,17 @@
+export interface InputWithLabelProps {
+  id: string;
+  label: string;
+  type?: "text" | "password" | "email" | "number";
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  placeholder?: string;
+  maxLength?: number;
+  autoComplete?: "on" | "off";
+  onFocus?: () => void;
+  onBlur?: () => void;
+  onButtonClick?: () => void;
+  noIcon?: boolean;
+  withBtn?: React.ReactNode;
+  className?: string;
+  showLength?: boolean;
+}
