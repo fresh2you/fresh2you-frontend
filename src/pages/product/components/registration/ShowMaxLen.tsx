2060b07f4062266e31cc4af7bf5535e4784f52cb
choycoy <passionfruits479@gmail.com>
2024-10-28 14:39:23 +0900

    FIX: 제품 등록 품 리팩토링
    - ts, tsx로 변환 및 비지니스 로직과 UI 로직 분리
    - 원래 쓰던 input을 공용 컴포넌트인 inputwithlabel로 변경
    - 수량을 +- controller가 아닌 input으로 입력하는 것으로 변경 및 수량도 세자리수마다 ,를 표시하는 것으로 변경

--- src/pages/product/components/registration/ShowMaxLen.tsx
+++ src/pages/product/components/registration/ShowMaxLen.tsx
@@ -1,6 +1,6 @@
 const ShowMaxLen: React.FC<{ value: string; maxLength: number | undefined }> = ({ value, maxLength }) => {
   return (
-    <span className="text-custom-btn-text text-gray-500 mt-0.5" aria-live="polite">
+    <span className="text-custom-btn-text text-gray-500 mt-1" aria-live="polite">
       {value.length}/{maxLength}
     </span>
   );
