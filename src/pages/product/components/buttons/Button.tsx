6a25469fa7d1ca2fa5c71e68298245bb76830b77
choycoy <passionfruits479@gmail.com>
2024-10-29 01:38:29 +0900

    FIX: 공용 컴포넌트 Button 재사용성 위해 변경

--- src/pages/product/components/buttons/Button.tsx
+++ src/pages/product/components/buttons/Button.tsx
@@ -4,14 +4,18 @@ type ButtonProps = {
   className?: string;
   text: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  type?: "button" | "submit" | "reset";
+  disabled?: boolean;
 };
 
-const Button: FC<ButtonProps> = ({ className = "", text, onClick }) => {
+const Button: FC<ButtonProps> = ({ className = "", text, onClick, type = "button", disabled = false }) => {
   return (
     <button
+      type={type}
       className={`mobile:py-1 px-2.5 rounded-md tablet-sm:py-1.5
         transition font-semibold ${className} custom-focus-light text-custom-btn-text whitespace-nowrap`}
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
