e45da3664b4a65a04ca389a55f6a35a7f6b55b5a
choycoy <passionfruits479@gmail.com>
2024-10-29 16:50:18 +0900

    FIX: 제품 등록 페이지 tsx로 변환 및 비지니스 로직 분리

---
+++ src/pages/product/components/registration/ProductForm.tsx
@@ -0,0 +1,65 @@
+import Textarea from "./Textarea";
+import DropdownSelect from "./DropDownSelect";
+import useFetchCategories from "../../hooks/useFetchCategories";
+import { handleInputChange } from "../../utils/productDataUtils";
+import InputWithLabel from "@/components/InputWithLabel";
+import { handleFieldChange } from "../../utils/productDataUtils";
+import handleRegistrationSubmit from "../../utils/handleRegistrationSubmit";
+import ProductImage from "./ProductImage";
+import RegistrationButtons from "./RegistrationButtons";
+import { useNavigate } from "react-router-dom";
+import useRegistrationLogic from "../../hooks/useRegistrationLogic";
+
+const ProductForm: React.FC = () => {
+  const { productData, setProductData, isFormValid } = useRegistrationLogic();
+  const { categories } = useFetchCategories();
+  const navigate = useNavigate();
+  const fields: { id: keyof ProductDataType; label: string; maxLength?: number; showLength?: boolean }[] = [
+    { id: "name", label: "상품명", maxLength: 15, showLength: true },
+    { id: "price", label: "가격" },
+    { id: "quantity", label: "수량" },
+  ];
+
+  return (
+    <form
+      onSubmit={(e) => handleRegistrationSubmit({ e, productData, navigate, isFormValid })}
+      className="flex flex-col gap-1"
+      aria-label="상품 등록 폼"
+      role="form"
+    >
+      {fields.map((field) => (
+        <InputWithLabel
+          key={field.id}
+          id={field.id}
+          label={field.label}
+          value={productData[field.id] as string}
+          onChange={(e) =>
+            field.id === "price" || field.id === "quantity"
+              ? handleInputChange(e, setProductData, field.id)
+              : handleFieldChange(field.id, e.target.value, setProductData)
+          }
+          maxLength={field.maxLength}
+          showLength={field.showLength}
+        />
+      ))}
+      <Textarea
+        id="description"
+        label="상품 설명"
+        value={productData.description}
+        onChange={(e) => handleFieldChange("description", e.target.value, setProductData)}
+        maxLength={500}
+      />
+      <DropdownSelect
+        id="category"
+        label="카테고리"
+        options={categories}
+        value={productData.categoryId}
+        onChange={(e) => handleFieldChange("categoryId", Number(e.target.value), setProductData)}
+      />
+      <ProductImage productData={productData} setProductData={setProductData} isFormValid={isFormValid} />
+      <RegistrationButtons isFormValid={isFormValid} navigate={navigate} />
+    </form>
+  );
+};
+
+export default ProductForm;
