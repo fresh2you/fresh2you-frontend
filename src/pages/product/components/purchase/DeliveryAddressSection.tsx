fc1b7ec0edc05c20c12d2bb8a8af1dd1aa8059f4
choycoy <passionfruits479@gmail.com>
2024-10-31 14:59:52 +0900

    FIX: [제품 구매 페이지], 주소관리하는 Modal 관련 컴포넌트

--- src/pages/product/components/purchase/DeliveryAddressSection.tsx
+++ src/pages/product/components/purchase/DeliveryAddressSection.tsx
@@ -3,19 +3,22 @@ import TextInput from "./TextInput";
 import { addressHandlers } from "../../utils/purchaseHandlers";
 import { useAtom } from "jotai";
 import { recipientDetailsAtom } from "../../atom/atom";
+import { overlay } from "overlay-kit";
+import AddressModal from "./AddressModal";
 
-interface DeliveryAddressSectionProps {
-  setShowAddressList: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-const DeliveryAddressSection: React.FC<DeliveryAddressSectionProps> = ({ setShowAddressList }) => {
+const DeliveryAddressSection = () => {
   const [recipientDetails, setRecipientDetails] = useAtom(recipientDetailsAtom);
+  const openAddressModal = () => {
+    overlay.open(({ isOpen, unmount }) => <AddressModal isOpen={isOpen} unmount={unmount} />, {
+      overlayId: "AddressModal",
+    });
+  };
   return (
     <section>
       <div className="flex items-center gap-4 mb-1.5">
         <label className="block font-semibold text-custom-input">배송지 주소</label>
         <Button
-          onClick={() => addressHandlers.handleOpenPostcode(setShowAddressList)}
+          onClick={openAddressModal}
           className="text-white bg-custom-green hover:bg-custom-green-hover"
           text="내 배송지 목록"
         />
