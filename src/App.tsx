fd055492fdd36065fdcda23436c5f62ad0e23fd1
JIHU96 <korjihu@gmail.com>
2024-11-02 02:08:22 +0900

    feat: react-query의 기본 옵션 추가
    - 창 포커스 시 재요청 비활성화
    - 데이터 기본 유효 시간을 5분으로 설정
    - 재요청 조건은 1번으로 적용

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,15 @@ import JotaiProvider from "./JotaiProvider";
 import { OverlayProvider } from "overlay-kit";
 
 const App = (): JSX.Element => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false, // 창 포커스 시 재요청 비활성화
+        staleTime: 5 * 60 * 1000, // 데이터 유효 시간을 5분으로 설정, 최초 실행 5분 후 서버에 데이터 재요청
+        retry: 1, // 쿼리함수가 error를 throw 시 한번만 재요청
+      },
+    },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
