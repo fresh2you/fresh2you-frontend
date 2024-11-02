2eaa72d286eb29e4ca656d44d0913a7315764139
JIHU96 <korjihu@gmail.com>
2024-11-03 00:24:46 +0900

    fix: 마이페이지의 메뉴 아이콘 교체

--- src/pages/mypage/mypage/hooks/useMyPageLogics.tsx
+++ src/pages/mypage/mypage/hooks/useMyPageLogics.tsx
@@ -1,8 +1,8 @@
 import IconHeart from "icons/heart.svg";
-import IconChange from "icons/change.svg";
+import IconChangePW from "icons/changePW.svg";
 import IconDelivery from "icons/delivery.svg";
 import IconLogout from "icons/logout.svg";
-import IconBox from "icons/carbonBox.svg";
+import IconRegister from "icons/register.svg";
 import IconList from "icons/product-list.svg";
 import { useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
@@ -34,19 +34,45 @@ const useMyPageLogics = () => {
   });
 
   const commonMenu = [
-    { id: 1, name: "찜 목록", path: "/mypage/likes", iconComponent: <IconHeart /> },
-    { id: 2, name: "내 배송지 관리", path: "/mypage/deliveries", iconComponent: <IconDelivery /> },
-    { id: 3, name: "비밀번호 변경", path: "/mypage/password", iconComponent: <IconChange /> },
-    { id: 4, name: "로그아웃", path: null, iconComponent: <IconLogout />, onClick: logout },
+    {
+      id: 1,
+      name: "찜 목록",
+      path: "/mypage/likes",
+      iconComponent: <IconHeart className="w-8 h-8 dark:text-white" />,
+    },
+    {
+      id: 2,
+      name: "내 배송지 관리",
+      path: "/mypage/deliveries",
+      iconComponent: <IconDelivery className="w-8 h-8 dark:text-white" />,
+    },
+    {
+      id: 3,
+      name: "비밀번호 변경",
+      path: "/mypage/password",
+      iconComponent: <IconChangePW className="w-8 h-8 dark:text-white" />,
+    },
+    {
+      id: 4,
+      name: "로그아웃",
+      path: null,
+      iconComponent: <IconLogout className="w-8 h-8 dark:text-white" />,
+      onClick: logout,
+    },
   ];
 
   const sellerMenu = [
-    { id: 1, name: "상품 등록", path: "/product/register", iconComponent: <IconBox /> },
+    {
+      id: 1,
+      name: "상품 등록",
+      path: "/product/register",
+      iconComponent: <IconRegister className="w-8 h-8 dark:text-white" />,
+    },
     {
       id: 2,
       name: "상품 목록 및 상품 수정",
       path: "/mypage/my-products",
-      iconComponent: <IconList />,
+      iconComponent: <IconList className="w-8 h-8 dark:text-white" />,
     },
   ];
 
