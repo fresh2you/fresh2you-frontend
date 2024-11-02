8dff0d7db9418e8777e7615150c56188ebd31572
JIHU96 <korjihu@gmail.com>
2024-11-03 00:43:02 +0900

    refactor: API 기본 응답 타입 중 data의 타입을 제네릭으로 변경

--- src/types/response/backend.d.ts
+++ src/types/response/backend.d.ts
@@ -1,10 +1,11 @@
 /* TODO: 추후 API 명세서 참고하여 수정 */
-declare interface defaultApiResponse {
+declare interface defaultApiResponse<T = null> {
   success: boolean;
   code: `${number}`;
   message: string;
-  data: null;
+  data: T;
 }
+
 declare interface ApiError {
   code: string;
   message: string;
