c6a62506bd58094a6790730f3fe763e54707eef1
choycoy <passionfruits479@gmail.com>
2024-11-02 14:34:44 +0900

    FIX: useHeaderProps 타입 변경

--- src/hooks/useHeaderProps.ts
+++ src/hooks/useHeaderProps.ts
@@ -1,11 +1,8 @@
 import { useEffect } from "react";
 import { useSetAtom } from "jotai";
 import { pageLayoutHeaderProps } from "@/stores/mypage";
-import { headerWithConfirm, headerWithoutConfirm } from "@/stores/mypage";
 
-type HeaderProps = headerWithConfirm | headerWithoutConfirm;
-
-const useHeaderProps = (props: HeaderProps) => {
+const useHeaderProps = (props: pageLayoutHeaderProps) => {
   const setHeaderProps = useSetAtom(pageLayoutHeaderProps);
 
   useEffect(() => {
