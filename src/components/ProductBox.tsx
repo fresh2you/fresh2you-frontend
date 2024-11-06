596fbcb1496821b083fa4295c38cb333138b1707
JIHU96 <korjihu@gmail.com>
2024-11-06 21:56:45 +0900

    feat: 검색/좋아요/판매상품 페이지에서 공통으로 쓰이는 ProductBox 컴포넌트에 수정/삭제 버튼을 옵셔널로 추가

--- src/components/ProductBox.tsx
+++ src/components/ProductBox.tsx
@@ -1,22 +1,18 @@
 import { Link } from "react-router-dom";
+import EditIcon from "@/assets/icons/edit.svg";
+import DeleteIcon from "@/assets/icons/bin.svg";
+import { ProductBoxProps } from "@/components/types/type.productBox";
 
-interface ProductBoxProps {
-  item: {
-    productId: number;
-    sellerName?: string;
-    productName: string;
-    productDescription?: string;
-    quantity?: number;
-    price: number;
-    imageUrl: string;
-  };
-}
-
-const ProductBox = ({ item }: ProductBoxProps) => {
+const ProductBox = ({
+  item,
+  hasOption = false,
+  editCallback = undefined,
+  deleteCallback = undefined,
+}: ProductBoxProps) => {
   return (
     <Link
       to={`/product/${item.productId}`}
-      className="w-full px-0 py-4 flex items-center gap-8 border-0 border-b-[1px] border-gray-300 rounded-none bg-white hover:text-inherit"
+      className="relative flex items-center w-full gap-6 px-0 py-4 bg-white border-gray-300 rounded-none hover:text-inherit border-b-[1px]"
     >
       <div className="h-20 bg-gray-400 rounded-lg aspect-square">
         {item.imageUrl && <img src={item.imageUrl} alt={item.productName} className="object-cover w-full h-full" />}
@@ -25,19 +21,49 @@ const ProductBox = ({ item }: ProductBoxProps) => {
       <div className="flex flex-col items-start justify-start w-full gap-1 py-1">
         <div className="flex flex-col items-start gap-1">
           <div className="text-base font-semibold">{item.productName}</div>
-          <div className="text-base font-medium text-left text-gray-500">{item.productDescription}</div>
+          {/* <div className="text-base font-medium text-left text-gray-500 line-clamp-2">{item.productDescription}</div> */}
         </div>
 
-        <div className="flex items-start h-full gap-1 tablet:self-end">
+        <div className="flex items-start h-full gap-1">
           {item.quantity && (
             <div className="text-base">
-              수량: {item.quantity}개<span className="mx-2 text-custom-gray-dark">|</span>
+              수량: {item.quantity.toLocaleString()}개<span className="mx-2 text-custom-gray-dark">|</span>
             </div>
           )}
 
           <div className="text-base">가격: {item.price.toLocaleString()}원</div>
         </div>
       </div>
+
+      {hasOption && (
+        <div className="absolute right-0 top-[20%] flex items-center gap-1">
+          {editCallback && (
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                // 수정 페이지로 이동
+                editCallback();
+              }}
+              className=" p-0 bg-inherit text-custom-gray-dark tablet:top-[30%] "
+            >
+              <EditIcon className="w-6 h-6 tablet:w-8 tablet:h-8" />
+            </button>
+          )}
+
+          {deleteCallback && (
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                // 삭제 로직
+                deleteCallback();
+              }}
+              className=" p-0 bg-inherit text-red-400 tablet:top-[30%]"
+            >
+              <DeleteIcon className="w-6 h-6 tablet:w-8 tablet:h-8" />
+            </button>
+          )}
+        </div>
+      )}
     </Link>
   );
 };
---
+++ src/components/types/type.productBox.ts
@@ -0,0 +1,31 @@
+interface BaseProductBoxProps {
+  item: {
+    productId: number;
+    sellerName: string;
+    productName: string;
+    productDescription: string;
+    quantity: number;
+    price: number;
+    imageUrl: string;
+  };
+  hasOption?: boolean;
+  editCallback?: () => void;
+  deleteCallback?: () => void;
+}
+
+type OptionTypes =
+  | {
+      hasOption: true;
+      editCallback: () => void;
+      deleteCallback?: () => void;
+    }
+  | {
+      hasOption: true;
+      editCallback?: () => void;
+      deleteCallback: () => void;
+    }
+  | {
+      hasOption: false;
+    };
+
+export type ProductBoxProps = BaseProductBoxProps & OptionTypes;
