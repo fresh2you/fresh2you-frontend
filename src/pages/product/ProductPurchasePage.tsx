6a3844187466b4bc49d2f8c4b6c84103bf733798
choycoy <passionfruits479@gmail.com>
2024-11-07 00:48:44 +0900

    fix: 토스 페이먼츠 UI 모달창에서 상품 구매 페이지내에서 렌더링 변경

--- src/pages/product/ProductPurchasePage.tsx
+++ src/pages/product/ProductPurchasePage.tsx
@@ -1,24 +1,26 @@
 import { useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import PurchaseForm from "./components/purchase/PurchaseForm";
 import { formatCurrency } from "../../utils/commonUtils";
 import { useFetchProductById } from "./hooks/useFetchProductById";
 import ProductSummary from "./components/purchase/ProductSummary";
-import { Loading } from "../redirection/component/Loading";
+import Loading from "../redirection/component/Loading";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import useHeaderProps from "@/hooks/useHeaderProps";
 import { useAtomValue } from "jotai";
 import { quantityAtom, recipientDetailsAtom } from "./atom/atom";
-import { toggleModal } from "./utils/purchaseHandlers";
 import { handleRenderPayment, handleDestroyWidget } from "./utils/handleTossPayments";
 import useWindowResize from "./hooks/useWindowResize";
-import PaymentModal from "./components/purchase/PaymentModal";
 import { usePurchaseProductByPoint } from "./hooks/usePurchaseProductByPoint";
 import { validatePurchaseData } from "./utils/purchaseHandlers";
 import PaymentOptionBtns from "./components/buttons/PaymentOptionBtns";
 import { useRef } from "react";
 import { WidgetPaymentMethodWidget } from "@tosspayments/tosspayments-sdk";
+import TwoActionBtns from "./components/buttons/TwoActionBtns";
+import { handleRequestTossPayment } from "./utils/handleTossPayments";
+import CurrentPointDisplay from "./components/purchase/CurrentPointDisplay";
+import usePaymentMethodReady from "./hooks/usePaymentMethodReady";
 
 const ProductPurchasePage = () => {
   const isDesktop = useWindowResize(1024);
@@ -37,15 +39,16 @@ const ProductPurchasePage = () => {
   const { fetchedProductById: product, isLoading } = useFetchProductById();
   const { mutate: purchaseProduct } = usePurchaseProductByPoint();
   const paymentWidgetRef = useRef<WidgetPaymentMethodWidget | null>(null);
+  const isPaymentMethodReady = usePaymentMethodReady(showPaymentMethod);
 
-  if (isLoading || !product) return <Loading isLayoutApplied={true} />;
+  if (isLoading || !product) return <Loading />;
 
   const totalAmount = product.price * quantity;
 
   return (
     <div
-      className={`text-custom-black mobile:w-11/12 max-w-[600px] flex flex-col pb-6 h-screen ${
-        !isDesktop ? "pt-2" : ""
+      className={`text-custom-black mobile:w-11/12 max-w-[600px] flex flex-col h-full pb-8 ${
+        !isDesktop ? "pt-4" : "pt-2"
       }`}
     >
       {isDesktop && (
@@ -56,30 +59,39 @@ const ProductPurchasePage = () => {
       <span className="mt-2 font-semibold text-center text-custom-p-lg">총 금액: {formatCurrency(totalAmount)} 원</span>
       <PaymentOptionBtns
         onPointPurchaseClick={() => {
-          const isValid = validatePurchaseData(recipientDetails, quantity);
+          const isValid = validatePurchaseData(recipientDetails, quantity, product.quantity);
           if (isValid) purchaseProduct({ recipientDetails, quantity, productId, product, navigate });
         }}
-        onGeneralPurchaseClick={() =>
-          handleRenderPayment(recipientDetails, setShowPaymentMethod, totalAmount, quantity, paymentWidgetRef)
-        }
+        onGeneralPurchaseClick={() => {
+          const isValid = validatePurchaseData(recipientDetails, quantity, product.quantity);
+          if (isValid)
+            handleRenderPayment(
+              recipientDetails,
+              setShowPaymentMethod,
+              totalAmount,
+              quantity,
+              paymentWidgetRef,
+              product.quantity,
+            );
+        }}
       />
-      <Link
-        className="mt-5 font-semibold text-center underline text-custom-gray-dark text-custom-sm-pn hover:text-custom-green"
-        to="/mypage/charge"
-      >
-        포인트 충전하러가기
-      </Link>
       {showPaymentMethod && (
-        <PaymentModal
-          product={product}
-          quantity={quantity}
-          unmount={() => {
+        <div id="payment-method" className="flex items-center justify-center h-full mt-4 rounded shadow-lg" />
+      )}
+      {isPaymentMethodReady && (
+        <TwoActionBtns
+          primaryText="결제하기"
+          primaryOnClick={() => handleRequestTossPayment({ product, quantity })}
+          secondaryText="닫기"
+          secondaryOnClick={() => {
+            setShowPaymentMethod(false);
             handleDestroyWidget(paymentWidgetRef);
-            toggleModal(setShowPaymentMethod);
           }}
+          extraClassName="bg-transparent mt-2"
         />
       )}
-      <ToastContainer />
+      {!showPaymentMethod && <CurrentPointDisplay />}
+      <ToastContainer limit={1} />
     </div>
   );
 };
