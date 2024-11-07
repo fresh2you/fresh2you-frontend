b7ab30a4e06470b78d937396ebeb1b729db8ee7f
JIHU96 <korjihu@gmail.com>
2024-11-07 19:25:17 +0900

    fix: 새로고침 등의 이유로 userInfo를 다시 불러오는 경우를 위해 point를 nullish 병합 연산자로 수정

--- src/pages/product/components/purchase/CurrentPointDisplay.tsx
+++ src/pages/product/components/purchase/CurrentPointDisplay.tsx
@@ -5,7 +5,7 @@ const CurrentPointDisplay = () => {
   const { userInfo } = useMyPageLogics();
   return (
     <div className="mt-5 text-center text-custom-sm-p">
-      <span className="font-semibold text-custom-green">현재 포인트: {userInfo.point}</span>
+      <span className="font-semibold text-custom-green">현재 포인트: {userInfo?.point}</span>
       <Link className="ml-3 font-semibold underline text-custom-gray-dark hover:text-custom-green" to="/mypage/charge">
         포인트 충전하러가기
       </Link>
